6825f7313c26dcb62e7d81fb5d4690467f22f864
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-03-31 19:45:39 +0000

    hacked up the printer and the syntax enough to parse basic stuff.  going to try and use kernel c inside of functions

--- ml-k/Kernel-C/parser/maudePrinter.ml
+++ ml-k/Kernel-C/parser/maudePrinter.ml
@@ -49,136 +49,76 @@ let sp : Pretty.doc = (text " ")
 
 let paren (d:Pretty.doc) : Pretty.doc = "(" ^+ d +^ ")"
 let giveType (d1:Pretty.doc) (d2:string) : Pretty.doc = paren(d1) ++ (text d2)
+let wrap (d1:Pretty.doc) (d2:string) : Pretty.doc = d2 ^+ paren(d1)
+
+(* let trim (s:string) : string = String.sub s 1 ((String.length s) - 1) *)
 
 class maudePrinter = object(self)
 	inherit defaultMaudePrinterClass as super
 	
-	method private pFunVariableDecl vi =
-		giveType (self#pVDecl () vi ++ text ";") ".Declaration"
-		
-	method private pVDecl_storage x = 
-		x
-	method private pType_parts d1 d2 d3 =
-		paren (d1 ++ sp ++ d2 ++ sp ++ d3)
-
-	method private pInstr_parts d1 d2 d3 d4 = 
-		(*text "(" ++ d1 ++ text ")" ++ text "(" ++ d2 ++ text ")" ++ text "(" ++ d3 ++ text ")"*)
-		d1 ++ paren (d2 ++ sp ++ d3 ++ sp ++ d4)
-		
-	(* Invoked for each variable declaration. Note that variable declarations are all the 
-	[GVar], [GVarDecl], [GFun], all the [varinfo] in formals of function types, and the 
-	formals and locals for function definitions. *)
-	(*method pVDecl () v = paren (super#pVDecl () v)(*giveType (super#pVDecl () v) ".Parameter-Declaration"*)*)
-
-	(* Invoked on each variable use. *)
-	(*method pVar (v:varinfo) : doc = giveType (super#pVar v) ".Identifier"*)
-    
-	(* Invoked on each lvalue occurence *)
-	method pLval () lv = paren(super#pLval () lv)
-
-	(*method pOffset: doc -> offset -> doc*)
-	(** Invoked on each offset occurence. The second argument is the base. *)
-
-	(* Invoked on each instruction occurrence. *)
-	method pInstr () i = paren (super#pInstr () i) (*giveType (super#pInstr () i) ".Statement"*)
-    
-	(* Control-flow statement. This is used by  {!Cil.printGlobal} and by {!Cil.dumpGlobal}. *)
-	method pStmt () st = paren(super#pStmt () st)
-
-	method pBlock () bl = paren(super#pBlock () bl)
-    (** Print a block. *)
-
-	(** Global (vars, types, etc.). This can be slow and is used only by 
-     * {!Cil.printGlobal} but by {!Cil.dumpGlobal} for everything else except 
-     * [GVar] and [GFun]. *)
-	method pGlobal () gl = paren ((paren (super#pGlobal () gl)) ++ text " ;; ")
-    
-	(** A field declaration *)
-	method pFieldDecl () fi = paren (super#pFieldDecl () fi)
-    
-	(** Print a statement kind. The code to be printed is given in the
-     * {!Cil.stmtkind} argument.  The initial {!Cil.stmt} argument
-     * records the statement which follows the one being printed;
-     * {!Cil.defaultCilPrinterClass} uses this information to prettify
-     * statement printing in certain special cases. *)
-	(* includes (return 0;) *)
-	method pStmtKind st () stk = paren(super#pStmtKind st () stk) (*: stmt -> unit -> stmtkind -> Pretty.doc*)
-    
-	
-	(** Print expressions *) 
-	method pExp () e = paren (super#pExp () e)
-    
-	
-	
-(*
-  method setCurrentFormals : varinfo list -> unit
-
-  method setPrintInstrTerminator : string -> unit
-  method getPrintInstrTerminator : unit -> string
-
-  method pVDecl: unit -> varinfo -> doc
-    (** Invoked for each variable declaration. Note that variable 
+	(** Invoked for each variable declaration. Note that variable 
      * declarations are all the [GVar], [GVarDecl], [GFun], all the [varinfo] 
      * in formals of function types, and the formals and locals for function 
      * definitions. *)
+	method pVDecl () (v:varinfo) = wrap (super#pVDecl () v) "Declaration"
+	
+	(** Print a block. *)
+	(* method pBlock () (b:block) = wrap (super#pBlock () b) "Block" *)
+    
+    (** A field declaration *)
+	(* method pFieldDecl () (f:fieldinfo) = wrap (super#pFieldDecl () f) "FieldDeclaration" *)
+	
+    (** Print initializers. This can be slow and is used by {!Cil.printGlobal} *)
+	method pInit () (i:init) = wrap (super#pInit () i) "Initializer"
+	
+    (** Invoked on each instruction occurrence. *)
+	(* method pInstr () (i:instr) = wrap (super#pInstr () i) "Instruction" *)
 
-  method pVar: varinfo -> doc
+	(** Invoked on each lvalue occurrence *)
+    (* method pLval () (l:lval) = wrap (super#pLval () l) "L-Value" *)
+	
+    (** Control-flow statement.  *)
+	(* method pStmt () (s:stmt) = wrap (super#pStmt () s) "ControlFlowStatement" *)
+	
+	(** Print a statement kind. The code to be printed is given in the
+	     * {!Cil.stmtkind} argument.  The initial {!Cil.stmt} argument
+	     * records the statement which follows the one being printed;
+	     * {!Cil.defaultCilPrinterClass} uses this information to prettify
+	     * statement printing in certain special cases. *)
+	(* method pStmtKind (s:stmt) () (k:stmtkind) = wrap (super#pStmtKind s () k) "StatementKind" *)
+    
+	(** Use of some type in some declaration. The first argument is used to print 
+		* the declared element, or is None if we are just printing a type with no 
+		* name being declared. Note that for structure/union and enumeration types 
+		* the definition of the composite type is not visited. Use [vglob] to 
+		* visit it.  *)
+	(* method pType (d:Pretty.doc option) () (t:typ) = wrap (super#pType d () t) "Type" *)
+	
+(*
+  method pVar: varinfo -> Pretty.doc
     (** Invoked on each variable use. *)
 
-  method pLval: unit -> lval -> doc
-    (** Invoked on each lvalue occurence *)
+  method pOffset: Pretty.doc -> offset -> Pretty.doc
+    (** Invoked on each offset occurrence. The second argument is the base. *)
 
-  method pOffset: doc -> offset -> doc
-    (** Invoked on each offset occurence. The second argument is the base. *)
+  method pLabel: unit -> label -> Pretty.doc
+    (** Print a label. *)
 
-  method pInstr: unit -> instr -> doc
-    (** Invoked on each instruction occurrence. *)
-
-  method pStmt: unit -> stmt -> doc
-    (** Control-flow statement. This is used by 
-     * {!Cil.printGlobal} and by {!Cil.dumpGlobal}. *)
-
-  method dStmt: out_channel -> int -> stmt -> unit
-    (** Dump a control-flow statement to a file with a given indentation. This is used by 
-     * {!Cil.dumpGlobal}. *)
-
-  method dBlock: out_channel -> int -> block -> unit
-    (** Dump a control-flow block to a file with a given indentation. This is 
-     * used by {!Cil.dumpGlobal}. *)
-
-  method pBlock: unit -> block -> Pretty.doc
-    (** Print a block. *)
-
-  method pGlobal: unit -> global -> doc
+  method pGlobal: unit -> global -> Pretty.doc
     (** Global (vars, types, etc.). This can be slow and is used only by 
-     * {!Cil.printGlobal} but by {!Cil.dumpGlobal} for everything else except 
-     * [GVar] and [GFun]. *)
+     * {!Cil.printGlobal} but not by {!Cil.dumpGlobal}. *)
 
-  method dGlobal: out_channel -> global -> unit
-    (** Dump a global to a file. This is used by {!Cil.dumpGlobal}. *)
+ 
 
-  method pFieldDecl: unit -> fieldinfo -> doc
-    (** A field declaration *)
-
-  method pType: doc option -> unit -> typ -> doc  
-  (* Use of some type in some declaration. The first argument is used to print 
-   * the declared element, or is None if we are just printing a type with no 
-   * name being declared. Note that for structure/union and enumeration types 
-   * the definition of the composite type is not visited. Use [vglob] to 
-   * visit it.  *)
-
-  method pAttr: attribute -> doc * bool
+  method pAttr: attribute -> Pretty.doc * bool
     (** Attribute. Also return an indication whether this attribute must be 
       * printed inside the __attribute__ list or not. *)
    
-  method pAttrParam: unit -> attrparam -> doc 
-    (** Attribute paramter *)
+  method pAttrParam: unit -> attrparam -> Pretty.doc 
+    (** Attribute parameter *)
    
-  method pAttrs: unit -> attributes -> doc
+  method pAttrs: unit -> attributes -> Pretty.doc
     (** Attribute lists *)
-
-  method pLabel: unit -> label -> doc
-    (** Label *)
 
   method pLineDirective: ?forcefile:bool -> location -> Pretty.doc
     (** Print a line-number. This is assumed to come always on an empty line. 
@@ -187,21 +127,9 @@ class maudePrinter = object(self)
      * is different from the last time time this function is called. The last 
      * file name is stored in a private field inside the cilPrinter object. *)
 
-  method pStmtKind : stmt -> unit -> stmtkind -> Pretty.doc
-    (** Print a statement kind. The code to be printed is given in the
-     * {!Cil.stmtkind} argument.  The initial {!Cil.stmt} argument
-     * records the statement which follows the one being printed;
-     * {!Cil.defaultCilPrinterClass} uses this information to prettify
-     * statement printing in certain special cases. *)
-
-  method pExp: unit -> exp -> doc
+  method pExp: unit -> exp -> Pretty.doc
     (** Print expressions *) 
 
-  method pInit: unit -> init -> doc
-    (** Print initializers. This can be slow and is used by 
-     * {!Cil.printGlobal} but not by {!Cil.dumpGlobal}. *)
-
-  method dInit: out_channel -> int -> init -> unit
-  *)
+*)	
 end
 
