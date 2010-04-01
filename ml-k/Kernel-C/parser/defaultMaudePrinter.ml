b1a765789a73e6663e4545bfdfad583076ca21db
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-04-01 19:37:53 +0000

    parses some simple functions with declaration things in prefix and function body in infix

--- ml-k/Kernel-C/parser/defaultMaudePrinter.ml
+++ ml-k/Kernel-C/parser/defaultMaudePrinter.ml
@@ -40,12 +40,322 @@
 open Cil
 open Pretty
 
+open Escape
+module E = Errormsg
+module H = Hashtbl
+module IH = Inthash
+
+let (+^) (d1:Pretty.doc) (d2:string) : Pretty.doc = d1 ++ text d2
+let (^+) (d1:string) (d2:Pretty.doc) : Pretty.doc = text d1 ++ d2
+let sp : Pretty.doc = (text " ")
+
+let paren (d:Pretty.doc) : Pretty.doc = "(" ^+ d +^ ")"
+let giveType (d1:Pretty.doc) (d2:string) : Pretty.doc = paren(d1) ++ (text d2)
+let wrap (d1:Pretty.doc) (d2:string) : Pretty.doc = d2 ^+ paren(d1)
+
 (* Pretty's api can be found in cil/ocamlutil/pretty.mli *)
-let pTypeSig : (typ -> typsig) ref =
-  ref (fun _ -> Errormsg.s (Errormsg.bug "pTypeSig not initialized"))
 
 class virtual defaultMaudePrinterClass = object (self) 
 	inherit defaultCilPrinterClass as super
+	
+	  val mutable currentFormals : varinfo list = []
+  method private getLastNamedArgument (s:string) : exp =
+    match List.rev currentFormals with 
+      f :: _ -> Lval (var f)
+    | [] -> 
+        E.s (bug "Cannot find the last named argument when printing call to %s\n" s)
 
+  method private setCurrentFormals (fms : varinfo list) =
+    currentFormals <- fms
+
+	
+method private pLvalPrec (contextprec: int) () lv = 
+    if getParenthLevel (Lval(lv)) >= contextprec then
+      text "(" ++ self#pLval () lv ++ text ")"
+    else
+      self#pLval () lv	
+	
+  (*** VARIABLES ***)
+  (* variable use *)
+  method pVar (v:varinfo) = text v.vname
+
+  (* variable declaration *)
+  method pVDecl () (v:varinfo) =
+    let stom, rest = separateStorageModifiers v.vattr in
+    (* First the storage modifiers *)
+    text (if v.vinline then "__inline " else "")
+      ++ d_storage () v.vstorage
+      ++ (self#pAttrs () stom)
+      ++ (self#pType (Some (text v.vname)) () v.vtype)
+      ++ text " "
+      ++ self#pAttrs () rest
+
+  (** Offsets **)
+  method pOffset (base: doc) = function
+    | NoOffset -> base
+    | Field (fi, o) -> 
+        self#pOffset (base ++ text "." ++ text fi.fname) o
+    | Index (e, o) ->
+        self#pOffset (base ++ text "[" ++ self#pExp () e ++ text "]") o
+
+  method pType (nameOpt: doc option) (* Whether we are declaring a name or 
+                                      * we are just printing a type *)
+               () (t:typ) =       (* use of some type *)
+    let name = match nameOpt with None -> nil | Some d -> d in
+    let printAttributes (a: attributes) = 
+      let pa = self#pAttrs () a in
+      match nameOpt with 
+      | None when not !print_CIL_Input && not !msvcMode -> 
+          (* Cannot print the attributes in this case because gcc does not 
+           * like them here, except if we are printing for CIL, or for MSVC. 
+           * In fact, for MSVC we MUST print attributes such as __stdcall *)
+          if pa = nil then nil else 
+          text "/*" ++ pa ++ text "*/"
+      | _ -> pa
+    in
+    match t with 
+      TVoid a -> text ""
+		++ paren (text "void")
+		++ if (name = nil) then (nil) else (text ", ")
+		++ (self#pAttrs () a)
+		++ text " " 
+		++ name
+
+    | TInt (ikind,a) -> text ""
+		++ paren (d_ikind () ikind)
+		++ text ", "
+		++ (self#pAttrs () a)
+		++ text " "
+		++ name
+
+    | TFloat(fkind, a) -> 
+        d_fkind () fkind 
+          ++ self#pAttrs () a 
+          ++ text " " 
+          ++ name
+
+    | TComp (comp, a) -> (* A reference to a struct *)
+        let su = if comp.cstruct then "struct" else "union" in
+        text (su ^ " " ^ comp.cname ^ " ") 
+          ++ self#pAttrs () a 
+          ++ name
+          
+    | TEnum (enum, a) -> 
+        text ("enum " ^ enum.ename ^ " ")
+          ++ self#pAttrs () a 
+          ++ name
+    | TPtr (bt, a)  -> 
+        (* Parenthesize the ( * attr name) if a pointer to a function or an 
+         * array. However, on MSVC the __stdcall modifier must appear right 
+         * before the pointer constructor "(__stdcall *f)". We push them into 
+         * the parenthesis. *)
+        let (paren: doc option), (bt': typ) = 
+          match bt with 
+            TFun(rt, args, isva, fa) when !msvcMode -> 
+              let an, af', at = partitionAttributes ~default:AttrType fa in
+              (* We take the af' and we put them into the parentheses *)
+              Some (text "(" ++ printAttributes af'), 
+              TFun(rt, args, isva, addAttributes an at)
+
+          | TFun _ | TArray _ -> Some (text "("), bt
+
+          | _ -> None, bt
+        in
+        let name' = text "*" ++ printAttributes a ++ name in
+        let name'' = (* Put the parenthesis *)
+          match paren with 
+            Some p -> p ++ name' ++ text ")" 
+          | _ -> name' 
+        in
+        self#pType 
+          (Some name'')
+          () 
+          bt'
+
+    | TArray (elemt, lo, a) -> 
+        (* ignore the const attribute for arrays *)
+        let a' = dropAttributes [ "const" ] a in 
+        let name' = 
+          if a' == [] then name else
+          if nameOpt == None then printAttributes a' else 
+          text "(" ++ printAttributes a' ++ name ++ text ")" 
+        in
+        self#pType 
+          (Some (name'
+                   ++ text "[" 
+                   ++ (match lo with None -> nil | Some e -> self#pExp () e)
+                   ++ text "]"))
+          ()
+          elemt
+          
+    | TFun (restyp, args, isvararg, a) -> 
+        let name' = 
+          if a == [] then name else 
+          if nameOpt == None then printAttributes a else
+          text "(" ++ printAttributes a ++ name ++ text ")" 
+        in
+        self#pType 
+          (Some
+             (wrap (name'
+                ++ text ", Parameter-Type-List("
+                ++ (align 
+                      ++ 
+                      (if args = Some [] && isvararg then 
+                        text "..."
+                      else
+                        (if args = None then nil 
+                        (* else if args = Some [] then text "void" *)
+						else if args = Some [] then (self#pType (None) () (TVoid []))
+                        else 
+                          let pArg (aname, atype, aattr) = 
+                            let stom, rest = separateStorageModifiers aattr in
+                            (* First the storage modifiers *)
+                            (self#pAttrs () stom)
+                              ++ (self#pType (Some (text aname)) () atype)
+                              ++ text " "
+                              ++ self#pAttrs () rest
+                          in
+                          (docList ~sep:(chr ',' ++ break) pArg) () 
+                            (argsToList args))
+                          ++ (if isvararg then break ++ text ", ..." else nil))
+                      ++ unalign)
+                ++ text ")") "Direct-Function-Declarator"))
+          ()
+          restyp
+
+  | TNamed (t, a) ->
+      text t.tname ++ self#pAttrs () a ++ text " " ++ name
+
+  | TBuiltin_va_list a -> 
+      text "__builtin_va_list"
+       ++ self#pAttrs () a 
+        ++ text " " 
+        ++ name
+	
+	
+	(***************************************************)
+	
+	(*** INSTRUCTIONS ****)
+  method pInstr () (i:instr) =       (* imperative instruction *)
+    match i with
+    | Set(lv,e,l) -> begin
+        (* Be nice to some special cases *)
+        match e with
+          BinOp((PlusA|PlusPI|IndexPI),Lval(lv'),Const(CInt64(one,_,_)),_)
+            when Util.equals lv lv' && one = Int64.one && not !printCilAsIs ->
+              self#pLineDirective l
+                ++ self#pLvalPrec indexLevel () lv
+                ++ text (" ++ " ^ self#getPrintInstrTerminator ())
+
+        | BinOp((MinusA|MinusPI),Lval(lv'),
+                Const(CInt64(one,_,_)), _) 
+            when Util.equals lv lv' && one = Int64.one && not !printCilAsIs ->
+                  self#pLineDirective l
+                    ++ self#pLvalPrec indexLevel () lv
+                    ++ text (" -- " ^ self#getPrintInstrTerminator ()) 
+
+        | BinOp((PlusA|PlusPI|IndexPI),Lval(lv'),Const(CInt64(mone,_,_)),_)
+            when Util.equals lv lv' && mone = Int64.minus_one 
+                && not !printCilAsIs ->
+              self#pLineDirective l
+                ++ self#pLvalPrec indexLevel () lv
+                ++ text (" -- " ^ self#getPrintInstrTerminator ())
+
+        | BinOp((PlusA|PlusPI|IndexPI|MinusA|MinusPP|MinusPI|BAnd|BOr|BXor|
+          Mult|Div|Mod|Shiftlt|Shiftrt) as bop,
+                Lval(lv'),e,_) when Util.equals lv lv' 
+                && not !printCilAsIs ->
+                  self#pLineDirective l
+                    ++ self#pLval () lv
+                    ++ text " " ++ d_binop () bop
+                    ++ text "= "
+                    ++ self#pExp () e
+                    ++ text (self#getPrintInstrTerminator ())
+                    
+        | _ ->
+            self#pLineDirective l
+              ++ self#pLval () lv
+              ++ text " = "
+              ++ self#pExp () e
+              ++ text (self#getPrintInstrTerminator ())
+              
+    end
+      (* In cabs2cil we have turned the call to builtin_va_arg into a 
+       * three-argument call: the last argument is the address of the 
+       * destination *)
+    | Call(None, Lval(Var vi, NoOffset), [dest; SizeOf t; adest], l) 
+        when vi.vname = "__builtin_va_arg" && not !printCilAsIs -> 
+          let destlv = match stripCasts adest with 
+            AddrOf destlv -> destlv
+              (* If this fails, it's likely that an extension interfered
+                 with the AddrOf *)
+          | _ -> E.s (E.bug 
+                        "%a: Encountered unexpected call to %s with dest %a\n" 
+                        d_loc l vi.vname self#pExp adest)
+          in
+          self#pLineDirective l
+	    ++ self#pLval () destlv ++ text " = "
+                   
+            (* Now the function name *)
+            ++ text "__builtin_va_arg"
+            ++ text "(" ++ (align
+                              (* Now the arguments *)
+                              ++ self#pExp () dest 
+                              ++ chr ',' ++ break 
+                              ++ self#pType None () t
+                              ++ unalign)
+            ++ text (")" ^ (self#getPrintInstrTerminator ()))
+
+      (* In cabs2cil we have dropped the last argument in the call to 
+       * __builtin_va_start and __builtin_stdarg_start. *)
+    | Call(None, Lval(Var vi, NoOffset), [marker], l) 
+        when ((vi.vname = "__builtin_stdarg_start" ||
+               vi.vname = "__builtin_va_start") && not !printCilAsIs) -> 
+        if currentFormals <> [] then begin
+		(*if (super#getLastNamedArgument ""; true) then begin*)
+          let last = self#getLastNamedArgument vi.vname in
+          self#pInstr () (Call(None,Lval(Var vi,NoOffset),[marker; last],l))
+        end
+        else begin
+          (* We can't print this call because someone called pInstr outside 
+             of a pFunDecl, so we don't know what the formals of the current
+             function are.  Just put in a placeholder for now; this isn't 
+             valid C. *)
+          self#pLineDirective l
+          ++ dprintf 
+            "%s(%a, /* last named argument of the function calling %s */)"
+            vi.vname self#pExp marker vi.vname
+          ++ text (self#getPrintInstrTerminator())
+        end
+      (* In cabs2cil we have dropped the last argument in the call to 
+       * __builtin_next_arg. *)
+    | Call(res, Lval(Var vi, NoOffset), [ ], l) 
+        when vi.vname = "__builtin_next_arg" && not !printCilAsIs -> begin
+          let last = self#getLastNamedArgument vi.vname in
+          self#pInstr () (Call(res,Lval(Var vi,NoOffset),[last],l))
+        end
+
+      (* In cparser we have turned the call to 
+       * __builtin_types_compatible_p(t1, t2) into 
+       * __builtin_types_compatible_p(sizeof t1, sizeof t2), so that we can
+       * represent the types as expressions. 
+       * Remove the sizeofs when printing. *)
+    | Call(dest, Lval(Var vi, NoOffset), [SizeOf t1; SizeOf t2], l) 
+        when vi.vname = "__builtin_types_compatible_p" && not !printCilAsIs -> 
+        self#pLineDirective l
+          (* Print the destination *)
+        ++ (match dest with
+              None -> nil
+            | Some lv -> self#pLval () lv ++ text " = ")
+          (* Now the call itself *)
+        ++ dprintf "%s(%a, %a)" vi.vname
+             (self#pType None) t1  (self#pType None) t2
+        ++ text (self#getPrintInstrTerminator())
+    | Call(_, Lval(Var vi, NoOffset), _, l) 
+        when vi.vname = "__builtin_types_compatible_p" && not !printCilAsIs -> 
+        E.s (bug "__builtin_types_compatible_p: cabs2cil should have added sizeof to the arguments.")
+          
+    | _ -> super#pInstr () i
+            
 	
 end (* class defaultCilPrinterClass *)
