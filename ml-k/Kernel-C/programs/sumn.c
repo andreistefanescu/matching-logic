1859e74c80f26106b9bcbefc3fb2e9cf5bef7179
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-04-12 22:03:43 +0000

    not sure what i've changed, but this is newer

--- ml-k/Kernel-C/programs/sumn.c
+++ ml-k/Kernel-C/programs/sumn.c
@@ -1,9 +1,15 @@
 int main() {
+	return sum(10);
+}
+
+int sum(int n) {
 	//@ assume < T > < env > .Map </ env > < mem > .Map </ mem > < form > @ </ form > </ T >
 	int sum = 0;
+	int i = 1;
 	//@ invariant .Bag
-	for(int i = 1; i <= 10; i++){
+	while (i <= n){
 		sum += i;
+		i++;
 	}
 	//@ assert .Bag
 	return sum;
