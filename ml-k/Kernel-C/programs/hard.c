a6568e748b697e7a66cf0660197320092f42be67
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-04-14 22:08:09 +0000

    make dynamic; make run   or   make match; make run    runs all the programs

--- ml-k/Kernel-C/programs/hard.c
+++ ml-k/Kernel-C/programs/hard.c
@@ -1,71 +1,72 @@
 //#include <stdio.h>
 #define N 16
 
-typedef int aaa, bbb, ccc;
+// typedef int aaa, bbb, ccc;
 
-int subfunction();
-void DoIt(float a, char b, char c)  __attribute__((cdecl));
-int thePtrFun(float x, char y, char z);
-float (*GetPtr1(const char opCode))(float, float);
-int test();
+// int subfunction();
+// void DoIt(float a, char b, char c)  __attribute__((cdecl));
+// int thePtrFun(float x, char y, char z);
+// float (*GetPtr1(const char opCode))(float, float);
+// int test();
 
-int main(int argc, char* argv[]) {
-	aaa n;           /* The current exponent */
-	int val = 1;     /* The current power of 2  */
+//int main(int argc, char* argv[]) {
+int main(void) {
+	// aaa n;           /* The current exponent */
+	// int val = 1;     /* The current power of 2  */
 	
-	int (*pt2Function)(float, char, char);
-	pt2Function = &thePtrFun;
+	// int (*pt2Function)(float, char, char);
+	// pt2Function = &thePtrFun;
 	
-	for (n=0; n<=N; n++) {
-		val = 2*val;
-	}
-	//printf(7);
-	//printf("%d\n", val);
-	test();
-	DoIt(0, 0, 0);
-	int result2 = (*pt2Function) (12, 'a', 'b');
-	float (*crazyGetPtr)(float, float) = (float (*)(float, float))(GetPtr1('+'));
-	int z = (*crazyGetPtr)(5, 10);
-	return val + z;
+	// for (n=0; n<=N; n++) {
+		// val = 2*val;
+	// }
+	// //printf(7);
+	// //printf("%d\n", val);
+	// test();
+	// DoIt(0, 0, 0);
+	// int result2 = (*pt2Function) (12, 'a', 'b');
+	// float (*crazyGetPtr)(float, float) = (float (*)(float, float))(GetPtr1('+'));
+	// int z = (*crazyGetPtr)(5, 10);
+	// return val + z;
 }
 
-int thePtrFun(float x, char y, char z){
-	return 0;
-}
+// int thePtrFun(float x, char y, char z){
+	// return 0;
+// }
 
-float first(float x, float y){
-	return x;
-}
-float second(float x, float y){
-	return y;
-}
+// float first(float x, float y){
+	// return x;
+// }
+// float second(float x, float y){
+	// return y;
+// }
 
-float (*GetPtr1(const char opCode))(float, float) {
-	if(opCode == '+') {
-		return &first;
-	} else {
-		return &second; // default if invalid operator was passed
-	}
-}
+// float (*GetPtr1(const char opCode))(float, float) {
+	// if(opCode == '+') {
+		// return &first;
+	// } else {
+		// return &second; // default if invalid operator was passed
+	// }
+// }
 
-int (*(*(*fun1(int opCode))(int, int))(int))(int) {
-	return 0;
-}
-int (*(*fun2(int opCode))(int, int))(int){
-	return 0;
-}
+// int (*(*(*fun1(int opCode))(int, int))(int))(int) {
+	// return 0;
+// }
+// int (*(*fun2(int opCode))(int, int))(int){
+	// return 0;
+// }
 
-int subfunction() {
-	int n;
-	return 15;
-}
+// int subfunction() {
+	// int n;
+	// return 15;
+// }
 
-const void doNothing() {
-}
+// const void doNothing() {
+// }
 
-int test(){
-	return 0;
-}
+// int test(){
+	// return 0;
+// }
 
-void DoIt(float a, char b, char c) {
-}
+// void DoIt(float a, char b, char c) {
+// }
