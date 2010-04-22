659a339da813e2570c1648906ed07e49bde48260
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-04-22 20:38:00 +0000

    much better handling of sizeof.  next, . and ->

--- trunk/ml-k/C/programs/multWithBreak.c
+++ trunk/ml-k/C/programs/multWithBreak.c
@@ -4,14 +4,14 @@ int main() {
 	int arr1[] = {1, 2, 3, 0, 4, 5} ;
 	int arr2[] = {1, 2, 3, 4, 5} ;
 	
-	for(int i = 0; i < sizeof(arr1); i++){
+	for(int i = 0; i < 6; i++){
 		product1 *= arr1[i];
 		if (product1 == 0){
 			break;
 		}
 	}
 	
-	for(int i = 0; i < sizeof(arr2); i++){
+	for(int i = 0; i < 5; i++){
 		product2 *= arr2[i];
 	}
 	
