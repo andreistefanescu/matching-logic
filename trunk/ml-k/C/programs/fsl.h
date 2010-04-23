cca48ddb703720f2df60ee2c8bd01433f0a992f0
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-04-23 00:19:40 +0000

    catching more identifiers with visitor

--- trunk/ml-k/C/programs/fsl.h
+++ trunk/ml-k/C/programs/fsl.h
@@ -1,9 +1,18 @@
 #define NULL 0
+#define EXIT_FAILURE 1
 typedef unsigned int size_t;
 
 // stdlib.h
 void *malloc(size_t size);
 void free(void *pointer);
+void *calloc(size_t nelem, size_t elsize);
+void exit(int status);
 
 // stdio.h
 int printf(const char *format, ...);
+
+// string.h
+size_t strlen( char *str );
+int strcmp( const char *str1, const char *str2 );
+char *strcpy(char *restrict s1, const char *restrict s2);
+
