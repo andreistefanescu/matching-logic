eac95abe41939b3baf9833c5bdc561b48195c40f
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-04-24 19:41:54 +0000

    rearranged comments

--- ml-k/C/programs/hashtable.c
+++ ml-k/C/programs/hashtable.c
@@ -1,4 +1,5 @@
 // based on code from http://en.literateprograms.org/Hash_table_(C)
+
 // Copyright (c) 2010 the authors listed at the following URL, and/or
 // the authors of referenced articles or incorporated external code:
 // http://en.literateprograms.org/Hash_table_(C)?action=history&offset=20100122102906
@@ -23,14 +24,18 @@
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // Retrieved from: http://en.literateprograms.org/Hash_table_(C)?oldid=16632
-//void main(void){}
 
 
 #ifndef HASHTBL_H_INCLUDE_GUARD
 #define HASHTBL_H_INCLUDE_GUARD
 
-//#include<stdlib.h>
 #include "fsl.h"
+
+//#include "hashtbl.h"
+//#include <string.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+
 
 typedef size_t hash_size;
 
@@ -59,36 +64,6 @@ int hashtbl_resize(HASHTBL *hashtbl, hash_size size);
 #endif
 
 
-// Copyright (c) 2010 the authors listed at the following URL, and/or
-// the authors of referenced articles or incorporated external code:
-// http://en.literateprograms.org/Hash_table_(C)?action=history&offset=20100122102906
-
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-// Retrieved from: http://en.literateprograms.org/Hash_table_(C)?oldid=16632
-
-
-//#include "hashtbl.h"
-
-//#include <string.h>
-//#include <stdio.h>
 
 static char *mystrdup(const char *s)
 {
@@ -262,34 +237,6 @@ int hashtbl_resize(HASHTBL *hashtbl, hash_size size)
 	return 0;
 }
 
-// Copyright (c) 2010 the authors listed at the following URL, and/or
-// the authors of referenced articles or incorporated external code:
-// http://en.literateprograms.org/Hash_table_(C)?action=history&offset=20100122102906
-
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-// Retrieved from: http://en.literateprograms.org/Hash_table_(C)?oldid=16632
-
-
-//#include"hashtbl.h"
-//#include<stdio.h>
 
 int main()
 {
