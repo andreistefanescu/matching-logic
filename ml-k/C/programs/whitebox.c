6440b269ac03b5657f0746d805ad5b7218076c75
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-04-26 23:52:18 +0000

    MUCH better handling of structs, typedefs.  fewer cases in looking up offset.  all programs run

--- ml-k/C/programs/whitebox.c
+++ ml-k/C/programs/whitebox.c
@@ -1,4 +1,16 @@
 #include "fsl.h"
+//#include <stdio.h>
+
+struct bigstruct {
+	int a;
+	int b;
+	int c;
+	int* p;
+	int d;
+	int e;
+	int f;
+	struct bigstruct* this;
+} ;
 
 int main(void){
 	int x = 1;
@@ -14,5 +26,21 @@ int main(void){
 	(**(ap + data[0]))++; // x == 5;
 	(*(ap[data[1]]))++; // x == 6;
 	
+	struct bigstruct s;
+	struct bigstruct* ps = &s;
+	s.a = 1;
+	s.b = 2;
+	s.c = 3;
+	s.d = 4;
+	s.e = 5;
+	s.f = 6;
+	s.p = &(s.b);
+	s.this = &s;
+	x += *((s.this)->p); // x == 8;
+	x += s.b + s.e + s.f; // x == 21;
+	(*((s.this)->p)) ++; // s.b == 3;
+	x += (&s)->b; // x == 24
+	
+	printf("%d\n",x);
 	return x;
 }
