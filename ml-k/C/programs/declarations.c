87c87930cc2fc62924ebeff36a0893c2410a9653
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-04-29 00:57:46 +0000

    harder declarations

--- ml-k/C/programs/declarations.c
+++ ml-k/C/programs/declarations.c
@@ -10,8 +10,22 @@ int* f(int x[]);
 	// struct str this;
 // } thatdef ;
 
+struct point {
+	int x;
+	int y;
+} ;
+struct s1 {
+	struct point a[2];
+} ;
+
+struct s2 {
+	struct s1 a;
+	struct s1 b;
+} ;
+
 
 int main(void){
+	int retval = 0;
 	int w;
 	int* x = &w;
 	*x = 5;
@@ -37,9 +51,20 @@ int main(void){
 	funcArr2D[0][0] = main;
 	funArrofArr[0] = f;
 	
+	struct s1 mys1;
+	struct s2 mys2;
+	mys1.a[0].x = 5;
+	mys1.a[0].y = -5;
+	mys1.a[1].x = 10;
+	mys1.a[1].y = -10;
+	mys2.a = mys1;
+	mys2.b = mys1;
+	
+	retval += mys2.a.a[0].x + mys2.a.a[1].y;
+	
 	// thatdef t;
 	// t.this.funcArr[0] = funcArr[0];
-	return 0;
+	return retval;
 }
 
 int* f(int x[]){
