204c018b229bcc0a00b26665b0bc3209a01c3863
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-05-05 19:04:20 +0000

    much better handling of types and declarations.  arr and helloworld run.  nearly finished with declarations.c, but need to figure out how to work with structs as values

--- trunk/ml-k/C/programs/declarations.c
+++ trunk/ml-k/C/programs/declarations.c
@@ -1,27 +1,27 @@
 #include "fsl.h"
 int* f(int x[]);
 
-// // typedef struct str {
-	// // int (*funcArr[2])(void);
-	// // int (*funcArr2D[2][2])(void);
-// // } strdef ;
+typedef struct str {
+	int (*funcArr[2])(void);
+	int (*funcArr2D[2][2])(void);
+} strdef ;
 
-// // typedef struct that {
-	// // struct str this;
-// // } thatdef ;
+typedef struct that {
+	struct str this;
+} thatdef ;
 
-// struct point {
-	// int x;
-	// int y;
-// } ;
-// struct s1 {
-	// struct point a[2];
-// } ;
+struct point {
+	int x;
+	int y;
+} ;
+struct s1 {
+	struct point a[2];
+} ;
 
-// struct s2 {
-	// struct s1 a;
-	// struct s1 b;
-// } ;
+struct s2 {
+	struct s1 a;
+	struct s1 b;
+} ;
 
 
 int main(void){
@@ -44,26 +44,28 @@ int main(void){
 	int* (*funArrofArr[2])(int x[]);
 	
 	d1[0] = 5;
-	// d2[0][0] = 5;
-	// d3[0][0][0] = 5;
+	d2[0][0] = 5;
+	d3[0][0][0] = 5;
 	
 	funcArr[0] = main;
-	// funcArr2D[0][0] = main;
-	// funArrofArr[0] = f;
+	funcArr2D[0][0] = main;
+	funArrofArr[0] = f;
 	
-	// struct s1 mys1;
-	// struct s2 mys2;
-	// mys1.a[0].x = 5;
-	// mys1.a[0].y = -5;
-	// mys1.a[1].x = 10;
-	// mys1.a[1].y = -10;
-	// mys2.a = mys1;
-	// mys2.b = mys1;
+	struct s1 mys1;
+	struct s2 mys2;
+	mys1.a[0].x = 5;
+	mys1.a[0].y = -5;
+	mys1.a[1].x = 10;
+	mys1.a[1].y = -10;
+	mys2.a = mys1;
+	mys2.b = mys1;
+	mys1.a[0].x = 30;
+	mys1.a[1].y = 30;
 	
-	// retval += mys2.a.a[0].x + mys2.a.a[1].y;
+	retval += mys2.a.a[0].x + mys2.a.a[1].y;
 	
-	// thatdef t;
-	// t.this.funcArr[0] = funcArr[0];
+	thatdef t;
+	t.this.funcArr[0] = funcArr[0];
 	return retval;
 }
 
