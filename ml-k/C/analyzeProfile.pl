daf909b4b3fc9b4de6fa7a12efff9908dfe11e12
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-05-13 20:31:08 +0000

    maude tool now generates csv formatted output

--- ml-k/C/analyzeProfile.pl
+++ ml-k/C/analyzeProfile.pl
@@ -2,17 +2,7 @@ use strict;
 use DBI;
 my $dbh = DBI->connect("dbi:SQLite:dbname=maudeProfileDBfile.sqlite","","");
 $dbh->do("DROP TABLE IF EXISTS data;");
-$dbh->do("CREATE TABLE data (rule NOT NULL, type NOT NULL, rewrites BIGINT NOT NULL, matches NULL DEFAULT NULL, fragment NULL DEFAULT NULL, initialTries NULL DEFAULT NULL, resolveTries NULL DEFAULT NULL, successes NULL DEFAULT NULL, failures NULL DEFAULT NULL)");
-#$dbh->do( "INSERT INTO data (rule, type, rewrites) VALUES ( 'test', 'op', 5 ) " );
- 
-# $dbh->do( "INSERT INTO authors VALUES ( 'Conway', 'Damian' ) " );
-# $dbh->do( "INSERT INTO authors VALUES ( 'Booch', 'Grady' ) " );
-# $dbh->do( "CREATE TABLE books ( title, author )" );
-# $dbh->do( "INSERT INTO books VALUES ( 'Object Oriented Perl',
-										 # 'Conway' ) " );
-# $dbh->do( "INSERT INTO books VALUES ( 'Object-Oriented Analysis and Design',
-										 # 'Booch' ) ");
-# $dbh->do( "INSERT INTO books VALUES ( 'Object Solutions', 'Booch' ) " );
+$dbh->do("CREATE TABLE data (rule NOT NULL, type NOT NULL, rewrites BIGINT NOT NULL, matches BIGINT, fragment NULL DEFAULT NULL, initialTries NULL DEFAULT NULL, resolveTries NULL DEFAULT NULL, successes NULL DEFAULT NULL, failures NULL DEFAULT NULL)");
 
 open(MYINPUTFILE, "<profile.log");
 my $line = <MYINPUTFILE>;
@@ -23,66 +13,108 @@ if (index($line,"\||||||||||||||||||/") != -1){
 	$line = <MYINPUTFILE>; $line = <MYINPUTFILE>;
 }
 
-#print "a\n";
 # after first ==========================================
-while($line = nextBlock(*MYINPUTFILE)) {
-	#print "$line\n";
+while(<MYINPUTFILE>) {
+	my $line = $_;
+	chomp($line);
 	if ($line =~ m/^op /){
 		handleOp($line, *MYINPUTFILE);
+	} elsif ($line =~ m/^eq /){
+		handleEq($line, *MYINPUTFILE, 'eq');
+	} elsif ($line =~ m/^ceq /){
+		handleCeq($line, *MYINPUTFILE, 'ceq');
+	} elsif ($line =~ m/^rl /){
+		handleEq($line, *MYINPUTFILE, 'rl');
+	} elsif ($line =~ m/^crl /){
+		handleCeq($line, *MYINPUTFILE, 'crl');
+	} else {
+		#print "--------------------\n$line\n--------------------\n";
 	}
 }
 
-# Print the line to the screen and add a newline
-my $res = $dbh->selectall_arrayref( q( 
-SELECT a.rule, a.type, a.rewrites 
+my $sth = $dbh->prepare("
+SELECT a.rule, a.type, a.rewrites, 
+	a.matches, a.fragment, a.initialTries, 
+	a.resolveTries,	a.successes, a.failures
 FROM data a
-ORDER BY a.rewrites DESC
-) );
-foreach(@$res) {
-	foreach my $i (0..$#$_) {
-		print "$_->[$i] "
-	}
-	print "\n";
+WHERE a.rewrites > 2
+ORDER BY a.matches DESC
+") or die $dbh->errstr;
+$sth->execute();
+print "Rule, Type, Rewrites, Matches, Fragment, Initial Tries, Resolve Tries, Successes, Failures\n";
+while (my $hash_ref = $sth->fetchrow_hashref) {
+	my $rule = substr($hash_ref->{rule}, 0, 100);
+	$rule =~ tr{\n}{ }; # turn newlines into spaces
+	$rule =~ s/["]/""/g; # escape quotes
+	my $type = $hash_ref->{type};
+	my $rewrites = $hash_ref->{rewrites};
+	my $matches = $hash_ref->{matches};
+	my $fragment = $hash_ref->{fragment};
+	my $initialTries = $hash_ref->{initialTries};
+	my $resolveTries = $hash_ref->{resolveTries};
+	my $successes = $hash_ref->{successes};
+	my $failures = $hash_ref->{failures};
+	print "\"$rule\", $type, $rewrites, $matches, $fragment, $initialTries, $resolveTries, $successes, $failures\n";
 }
-
 $dbh->disconnect;
 
-sub nextBlock {
-	my ($file) = @_;
-	while(<$file>) {
-		my $line = $_;
-		chomp($line);
-		if ($line =~ m/^op /){
-			return $line;
-		} else {
-			next;
-		}
-	}
-	return 0;
-}
 
+#############################################
 sub handleOp {
 	my ($line, $file) = @_;
-	if ($line eq "") { return; }
-	my $op = substr($line, 3);
+	my $rule = substr($line, 3);
 	$line = <$file>;
-	if ($line =~ m/: (\d+) \(/){
+	if ($line =~ m/built-in eq rewrites: (\d+) \(/){
 		my $rewrites = $1;
-		$dbh->do( "INSERT INTO data (rule, type, rewrites) VALUES ( '$op', 'op', $rewrites ) " );
+		my $sql = "INSERT INTO data (rule, type, rewrites) VALUES (?, ?, ?)";
+		my $sth = $dbh->prepare($sql);
+		$sth->execute($rule, 'op', $rewrites);
+		return;
 	}
-	
-	
-	# while(<$file>) {
-		# my $line = $_;
-		# chomp($line);
-		# if ($line =~ m/^op /){
-			# return $line;
-		# } else {
-			# next;
-		# }
-	# }
-	
-	#
-	
-	return 0;
 }
+sub handleEq {
+	my ($line, $file, $type) = @_;
+	my $rule = $line;
+	while (<$file>){
+		$line = $_;
+		chomp($line);
+		if ($line =~ m/^rewrites: (\d+) \(/){
+			my $rewrites = $1;
+			my $sql = "INSERT INTO data (rule, type, rewrites, matches) VALUES (?, ?, ?, ?)";
+			my $sth = $dbh->prepare($sql);
+			$sth->execute($rule, $type, $rewrites, $rewrites);
+			return;
+		} else {
+			$rule .= "$line\n";
+		}
+	}
+}
+sub handleCeq {
+	my ($line, $file, $type) = @_;
+	my $rule = $line;
+	while (<$file>){
+		$line = $_;
+		chomp($line);
+		if ($line =~ m/^lhs matches: (\d+)	rewrites: (\d+) \(/){
+			my $matches = $1;
+			my $rewrites = $2;
+			$line = <$file>;
+			$line = <$file>;
+			chomp($line);
+			$line =~ m/^(\d+)\t\t(\d+)\t\t(\d+)\t\t(\d+)\t\t(\d+)/;
+			my $fragment = $1;
+			my $initialTries = $2;
+			my $resolveTries = $3;
+			my $successes = $4;
+			my $failures = $5;
+			
+			my $sql = "INSERT INTO data (rule, type, rewrites, matches, fragment, initialTries, resolveTries, successes, failures) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			my $sth = $dbh->prepare($sql);
+			$sth->execute($rule, $type, $rewrites, $matches, $fragment, $initialTries, $resolveTries, $successes, $failures);
+			return;
+		} else {
+			$rule .= "$line\n";
+		}
+	}
+}
+
