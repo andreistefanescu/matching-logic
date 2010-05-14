7b969b69cd8bf7e1ab80746cf323b3d6e5917860
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-05-14 16:09:38 +0000

    schorr-wait runs

--- trunk/ml-k/C/programs/schorr-waite.c
+++ trunk/ml-k/C/programs/schorr-waite.c
@@ -53,6 +53,11 @@ node setLeft(node curr, node left){
 	curr->l = left;
 	return left;
 }
+node setRight(node curr, node right){
+	printf("Setting right child of %d to %d\n", curr->value, right->value);
+	curr->r = right;
+	return right;
+}
 
 // algorithm  dft(x) 
    // visit(x) 
@@ -61,11 +66,48 @@ node setLeft(node curr, node left){
         // dft(y)
 		
 void showGraph(node this){
-	
+	if (this == NULL) { return; }
+	if (this->v) { return; }
+	printf("%d:m=%d:l=%d:r=%d\n", this->value, this->m, this->l->value, this->r->value);
+	this->v = 1;
+	showGraph(this->l);
+	showGraph(this->r);
+}
+
+void cleanGraph(node this){
+	if (this == NULL) { return; }
+	if (this->v) {
+		this->v = 0;
+		this->m = 0;
+		this->c = 0;
+		cleanGraph(this->l);
+		cleanGraph(this->r);
+	}
 }
 
 int main(void) {
-	node root = createNode();
-	setLeft(root, createNode());
-
+	node a, b, c, d, e;
+	a = createNode();
+	b = createNode();
+	c = createNode();
+	d = createNode();
+	e = createNode();
+	setLeft(a, b);
+	setRight(a, c);
+	setLeft(b, d);
+	setRight(b, d);
+	setLeft(c, b);
+	setRight(c, a);
+	setLeft(d, c);
+	setRight(d, e);
+	setLeft(e, e);
+	setRight(e, a);
+	
+	node root = a;
+	showGraph(root);
+	cleanGraph(root);
+	printf("Now running Schorr-Waite...\n");
+	
+	schorr_waite(root);
+	showGraph(root);
 }
