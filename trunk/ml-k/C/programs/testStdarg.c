d9d6c2a1e86896aae8a26cce9c7f09e142d37c0a
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-05-19 23:29:21 +0000

    basic stdarg seems to work.  need more tests with varying sizes

---
+++ trunk/ml-k/C/programs/testStdarg.c
@@ -0,0 +1,63 @@
+// #include <stdio.h>
+// #include <stdlib.h>		/* for malloc, NULL, size_t */
+// #include <stdarg.h>		/* for va_ stuff */
+// #include <string.h>		/* for strcat et al. */
+#include "fsl.h"
+ 
+void printargs(int arg1, ...) /* print all int type args, finishing with -1 */
+{
+  va_list ap;
+  int i;
+ 
+  va_start(ap, arg1); 
+  for (i = arg1; i != -1; i = va_arg(ap, int))
+    printf("%d ", i);
+  va_end(ap);
+  putchar('\n');
+}
+ 
+
+char *vstrcat(const char *first, ...)
+{
+	size_t len;
+	char *retbuf;
+	va_list argp;
+	char *p;
+
+	if(first == NULL)
+		return NULL;
+
+	len = strlen(first);
+	va_start(argp, first);
+
+	while((p = va_arg(argp, char *)) != NULL)
+		len += strlen(p);
+
+	va_end(argp);
+	retbuf = malloc(len + 1);	/* +1 for trailing \0 */
+
+	if(retbuf == NULL)
+		return NULL;		/* error */
+
+	(void)strcpy(retbuf, first);
+	va_start(argp, first);		/* restart; 2nd scan */
+
+	while((p = va_arg(argp, char *)) != NULL)
+		(void)strcat(retbuf, p);
+
+	va_end(argp);
+	return retbuf;
+}
+ 
+
+int main(void) {
+	printargs(5, 2, 14, 84, 97, 15, 24, 48, -1);
+	printargs(84, 51, -1);
+	printargs(-1);
+	printargs(1, -1);
+
+	char *str = vstrcat("Hello, ", "world!", "1", "23", "456", "789", (char *)NULL);
+	puts(str);
+
+	return 0;
+}
