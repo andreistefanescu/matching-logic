578790e2287142bc6dfc68f90f2d6ce4808c620a
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-05-24 23:07:52 +0000

    redid the way programs are compiled again.  MUCH faster compilation.  get one file per program

--- ml-k/C/programs/sumn.c
+++ ml-k/C/programs/sumn.c
@@ -6,15 +6,15 @@ int main() {
 }
 
 int sum(int n) {
-	//@ pre < T > < env > @n |-> ?l1 </ env > < mem > ?l1 |-> #n0 </ mem > < form > TrueFormula </ form > </ T >
+	//@xxx pre < T > < env > @n |-> ?l1 </ env > < mem > ?l1 |-> #n0 </ mem > < form > TrueFormula </ form > </ T >
 	int sum = 0;
 	int i = 1;
-	//@ invariant < T > < env > @n |-> ?l1 @sum |-> ?l2 </ env > < mem > ?l1 |-> ?n ?l2 |-> (((#n0 +Int (-Int ?n)) *Int (#n0 +Int ?n +Int 1)) /Int 2) </ mem > < form > TrueFormula </ form > </ T >
+	//@xxx invariant < T > < env > @n |-> ?l1 @sum |-> ?l2 </ env > < mem > ?l1 |-> ?n ?l2 |-> (((#n0 +Int (-Int ?n)) *Int (#n0 +Int ?n +Int 1)) /Int 2) </ mem > < form > TrueFormula </ form > </ T >
 	// xx @(?n >=Int 0) /\ @(#n0 >=Int 0)
 	while (i <= n){
 		sum += i;
 		i++;
 	}
-	//@ post .Bag
+	//@xxx post .Bag
 	return sum;
 }
