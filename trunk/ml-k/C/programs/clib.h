8ced1b1658e3d361acb758ad5a6036dfc74775c7
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-05-30 02:00:19 +0000

    better implementation of lib function since it messed up on base64

--- trunk/ml-k/C/programs/clib.h
+++ trunk/ml-k/C/programs/clib.h
@@ -1,6 +1,6 @@
 #define NULL 0
 typedef unsigned long int size_t;
-
+// nice pd implementations at http://en.wikibooks.org/wiki/C_Programming/Strings
 char *strcpy(char *dest, const char *src)
 {
   unsigned i;
@@ -32,20 +32,15 @@ int strcmp (const char * s1, const char * s2) {
     return *(unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1;
 }
 
-char* strchr(const char *s, int c){
-	int i = 0;
-	if (s == NULL){
-		return NULL;
-	}
-	while (1){
-		if (s[i] == c){
-			return (char*)(&(s[i]));
-		} else if (s[i] == 0){
-			return NULL;
-		}
-		i++;
-	}
+char *(strchr)(const char *s, int c) {
+ /* Scan s for the character.  When this loop is finished,
+	s will either point to the end of the string or the
+	character we were looking for.  */
+ while (*s != '\0' && *s != (char)c)
+	 s++;
+ return ( (*s == c) ? (char *) s : NULL );
 }
+
 
 // from http://www.danielvik.com/2010/02/fast-memcpy-in-c.html
 // by Daniel Vik
