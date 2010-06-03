37d95b9fc3ba43d6b202c3e52e16a9d1678e5c94
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-06-03 01:07:26 +0000

    another example and added sin

--- trunk/ml-k/C/programs/math.h
+++ trunk/ml-k/C/programs/math.h
@@ -1,7 +1,8 @@
 // math.h (real c99 needs -lm to get math library linked in)
 double sqrt (double x);
 double cos(double x);
-int abs( int num );
+double sin(double x);
+int abs(int num);
 double fabs( double arg );
 double pow(double x, double  y);
 double exp(double x);
