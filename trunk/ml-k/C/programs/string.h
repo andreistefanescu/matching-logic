f74e912308efd9931ab3dd6fbeb9097b896ade98
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-06-03 18:33:21 +0000

    some small changes to make gcc happier and code prettier

--- trunk/ml-k/C/programs/string.h
+++ trunk/ml-k/C/programs/string.h
@@ -3,7 +3,7 @@
 typedef unsigned long int size_t; // this needs to correspond to cfg:sizeut
 size_t strlen(char *str);
 int strcmp(const char *str1, const char *str2);
-char* strcpy(char *restrict s1, const char *restrict s2);
+char* strcpy(char* s1, const char* s2);
 char* strcat(char* dest, const char* src);
 char* strchr(const char *s, int c);
 void * memset ( void * ptr, int value, size_t num );
