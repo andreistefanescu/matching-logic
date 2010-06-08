93215e85121e529b7e508db31aebc260bf898abe
cmelliso <cmelliso@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2010-06-08 07:28:28 +0000

    fixed casts for function calls.  a few more things parse, all but two gcc parse, all tests pass

--- trunk/ml-k/C/gcc-test/gcc.8th/20030221-1.c
+++ trunk/ml-k/C/gcc-test/gcc.8th/20030221-1.c
@@ -1,14 +1,14 @@
 /* PR optimization/8613 */
 /* Contributed by Glen Nakamura */
-
+#include <string.h>
 extern void abort (void);
- 
+
 int main (void)
 {
   char buf[16] = "1234567890";
   char *p = buf;
 
-  *p++ = (char) __builtin_strlen (buf);
+  *p++ = (char) strlen (buf);
 
   if ((buf[0] != 10) || (p - buf != 1))
     abort ();
