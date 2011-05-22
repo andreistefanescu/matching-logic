a1efaa21cceda0e2c15fd26788ae5c2a0cbce20e
andreistef <andreistef@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2011-05-22 01:18:02 +0000

    licence added

--- matchC/bin/matchC.py
+++ matchC/bin/matchC.py
@@ -1,10 +1,35 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
+
+'''
+    This file is part of the Matching Logic Verification Framework
+
+    Copyright (C) 2009-2011 Grigore Rosu
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+'''
+
+
+import sys
+if sys.version_info[0:2] < (3,2):
+    print("matchC requires Python 3.2 or higher")
+    exit(1)
 
 import argparse
 import os
 import platform
 import subprocess
-import sys
 import tempfile
 import time
 
