1022ebc3fa19b0759206f593167b883fc7601f42
naum.elena@gmail.com <naum.elena@gmail.com@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2011-07-07 13:52:23 +0000

    data backup

---
+++ branches/mC/svnGenerator/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+gcc -C -E $1 &>/dev/null
+
+if [ $? -ne 0 ]
+then
+{
+    echo "The file outputs errors when being compiled using gcc, please review your code!"
+    exit 1
+}
+else
+{
+echo "success"
+}
+fi
+
+make
+make run ARG=$1
