2f98a77558493b809ef0c39bc2878411a7403393
andreistef <andreistef@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2011-08-24 14:18:42 +0000

    tree schorr-waite

--- trunk/matchC/developing_examples/schorr_waite.c
+++ trunk/matchC/developing_examples/schorr_waite.c
@@ -8,12 +8,22 @@ struct graphNode {
 };
 
 
-void schorr_waite(struct graphNode *root)
+void schorr_waite_tree(struct graphNode *root)
+/*@ rule <k> $ => return; ...</k>
+         <heap>... swtree(root)(T) => swtree(root)(?T) ...</heap>
+    if isConst(0, proj(1, T)) /\ isConst(3, proj(1, ?T))
+       /\ proj(0, T) = proj(0, ?T) */
 {
   struct graphNode *p;
   struct graphNode *q;
 
+  if (root != NULL)
+    root->mark;
+
   p = root; q = NULL;
+  /*@ inv <heap>... swtree(p)(?TP), swtree(q)(?TQ) ...</heap>
+          /\ isSWMarkedPath(proj(1, ?TP), proj(1, ?TQ))
+          /\ proj(0, T) = SWPath2ptrTree(?TP, ?TQ) */
   while (p != NULL) {
     struct graphNode *t;
 
@@ -32,11 +42,53 @@ void schorr_waite(struct graphNode *root)
       q = t;
     }
   }
+
+  if (q != NULL)
+    q->mark;
 }
 
 
-int main() { }
+void schorr_waite_graph(struct graphNode *root)
+/* rule <k> $ => return; ...</k>
+         <heap>... swgraph(root, empty)(G) => swgraph(root, empty)(?G) ...</heap>
+    if isConst(0, proj(1, G)) /\ isConst(3, proj(1, ?G))
+       /\ proj(0, T) = proj(0, ?T) */
+{
+  struct graphNode *p;
+  struct graphNode *q;
 
+  if (root != NULL)
+    root->mark;
+
+  p = root; q = NULL;
+  /* inv <heap>... swtree(p)(?TP), swtree(q)(?TQ) ...</heap>
+          /\ isSWMarkedPath(proj(1, ?TP), proj(1, ?TQ))
+          /\ proj(0, T) = SWPath2ptrTree(?TP, ?TQ) */
+  while (p != NULL) {
+    struct graphNode *t;
+
+    p->mark = p->mark + 1;
+    if (p->mark == 3 || p->left != NULL && p->left->mark == 0) {
+      t = p->left;
+      p->left = p->right;
+      p->right = q;
+      q = p;
+      p = t;
+    }
+    else {
+      t = p->left;
+      p->left = p->right;
+      p->right = q;
+      q = t;
+    }
+  }
+
+  if (q != NULL)
+    q->mark;
+}
+
+
+//@ var T, TP, TQ : Tree
 
 /*
 eq Schorr-Waite-tree =
