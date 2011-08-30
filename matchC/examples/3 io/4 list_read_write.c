2d170646146de0f80a930ef867d8f3de6ae14ff8
grigore.rosu <grigore.rosu@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2011-08-30 02:59:39 +0000

    io directory commented

--- matchC/examples/3 io/4 list_read_write.c
+++ matchC/examples/3 io/4 list_read_write.c
@@ -3,7 +3,7 @@
  * writes them to the standard output in reverse order.  To do so, it
  * first stores the n elements in a singly-linked list, which it allocates
  * as the elements are read, and then it outputs the n elements from the
- * list, at the same time deallocating the list.  In the end, the heap
+ * list, at the same time dealocating the list.  In the end, the heap
  * stays unchanged (the heap does not appear in the function specification).
  */
 
