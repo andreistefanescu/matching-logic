917e56e89eedef11c7236720a8f1aaf86f38c2a3
grigore.rosu <grigore.rosu@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2011-08-30 02:55:35 +0000

    io directory commented

--- trunk/matchC/examples/3 io/4 list_read_write.c
+++ trunk/matchC/examples/3 io/4 list_read_write.c
@@ -1,9 +1,9 @@
 /*
  * Function list_read_write reads n elements from the standard input and
- * writes them to the standard output in reversed order.  To do so, it
- * first stores the n elements in a singly-linked list which it allocates
+ * writes them to the standard output in reverse order.  To do so, it
+ * first stores the n elements in a singly-linked list, which it allocates
  * as the elements are read, and then it outputs the n elements from the
- * list at the same time deallocating the list.  In the end, the heap
+ * list, at the same time deallocating the list.  In the end, the heap
  * stays unchanged (the heap does not appear in the function specification).
  */
 
