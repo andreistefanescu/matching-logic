8df9b07cd979d0266d3ece48470dd86721d94270
naum.elena@gmail.com <naum.elena@gmail.com@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2011-08-30 07:52:01 +0000

    reverted from cons to add

---
+++ trunk/matchC/examples/2 list/add.c
@@ -0,0 +1,30 @@
+/*
+ * Function list_add appends an integer value to a singly-linked list.
+ */
+
+
+#include <stdlib.h>
+
+
+struct listNode {
+  int val;
+  struct listNode *next;
+};
+
+
+struct listNode* list_add(int v, struct listNode* x)
+/*@ rule <k> $ => return ?x; ...</k> 
+         <heap>... list(x)(A) => list(?x)([v] @ A) ...</heap> */
+{
+  struct listNode* y;
+
+  y = (struct listNode*) malloc (sizeof(struct listNode));
+  y->val = v;
+  y->next = x;
+
+  return y;
+}
+
+
+//@ var A : Seq
+
