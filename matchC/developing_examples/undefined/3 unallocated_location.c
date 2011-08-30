98707a62030a2f9d2423d1c6d1d627cbe83eaa56
grigore.rosu <grigore.rosu@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2011-08-30 20:41:47 +0000

    undefined examples revised

--- matchC/developing_examples/undefined/3 unallocated_location.c
+++ matchC/developing_examples/undefined/3 unallocated_location.c
@@ -1,6 +1,8 @@
 /*
  * The program below accesses a location which has not been allocated.
  * gcc compiles it, but the generated binary yields a segmentation fault.
+ * MatchC's error message shows how it got stuck on attempting to "derive"
+ * (i.e., make available) memory location 1000, in order to read an int from it.
  */
 
 
