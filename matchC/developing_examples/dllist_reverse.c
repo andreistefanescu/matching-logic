100127e4806cf74e3c5c47d83e372692d09f9f05
naum.elena <naum.elena@gmail.com>
2011-08-31 09:48:15 +0000

    dllist reverse

---
+++ matchC/developing_examples/dllist_reverse.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct dllistNode {
+  int val;
+  struct dllistNode *next;
+  struct dllistNode *prev;
+};
+
+struct dllistNode* reverse(struct dllistNode* a)
+/*@ rule <k> $ => return ?a; ...</k>
+         <heap>... dllist(a)(A) => dllist(?a)(rev(A)) ...</heap> */
+{
+  struct dllistNode* p;
+  struct dllistNode* aux;
+  
+  p = 0;
+  
+  if ((a->next == 0) && (a->prev == 0)) {
+    p = a;
+  }
+  else {
+    p = a;
+    
+    a = a->next;
+    a->prev = 0;
+    
+    p->next = 0;
+    p->prev = 0;
+  /*@ inv <heap>... dllseg(0,old(a))(a,?y,?A1), 
+                    dllseg(a,?y)(?z,0,?A2) ...</ heap >
+          /\ A = ?A1 @ ?A2 */
+    while(a!=0)
+    {
+      aux = a->next;
+      a->next = p;
+      a->prev = 0;
+      p->prev = a;
+      p = a;
+      a = aux;
+    }
+  }
+  return p;
+}
+
+
+void print(struct dllistNode* a)
+{
+  struct dllistNode* aux;
+  aux = a;
+  printf("\n");
+  while(aux!=0)
+  {
+    printf("%d ", aux->val);
+    aux = aux->next;
+  }
+  printf("\n");
+}
+
+int main()
+{
+  struct dllistNode* x;
+  x = (struct dllistNode*)malloc(sizeof(struct dllistNode));
+  x->val = 5;
+  x->next = 0;
+  x->prev = 0;
+  print(x);
+  x = reverse(x);
+  print(x);
+  return 0;
+}
+
+
+//@ var y, z : Int
+//@var A : Seq
