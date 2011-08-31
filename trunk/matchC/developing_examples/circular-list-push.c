75620898dc87e72da2807c7dbf9e5704d1fd0aa5
naum.elena@gmail.com <naum.elena@gmail.com@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2011-08-31 10:45:14 +0000

    circular list push

--- trunk/matchC/developing_examples/circular-list-push.c
+++ trunk/matchC/developing_examples/circular-list-push.c
@@ -25,23 +25,5 @@ struct listNode* clpush(struct listNode* x, int val)
   return x;
 }
 
-
-int main()
-{
-  struct listNode *x;
-  struct listNode *y;
-  x = (struct listNode*)malloc(sizeof(struct listNode));
-  x->val = 5;
-  x->next = 0;
-  y = (struct listNode*)malloc(sizeof(struct listNode));
-  y->val = 8;
-  y->next = x;
-  x->next = y;
-  printf("%d %d\n", x->val, y->val);
-  x = clpush(x,9);
-  printf("%d %d %d\n", x->val, x->next->val, x->next->next->val);
-  return 0;
-}
-
 //@ var aux, n, v : Int
 //@ var A : Seq
