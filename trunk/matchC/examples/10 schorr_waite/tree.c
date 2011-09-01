b242f8f8b6b240d837d097f488af17cced569747
andreistef <andreistef@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2011-09-01 15:11:03 +0000

    minor schorr_waite/tree comments

--- trunk/matchC/examples/10 schorr_waite/tree.c
+++ trunk/matchC/examples/10 schorr_waite/tree.c
@@ -1,17 +1,17 @@
 /*
  * Function schorr_waite implements the Schorr-Waite graph marking algorithm, as
- * described in [1].  Briefly, the algorithm traverses the graph in depth-first
- * search order and marks the nodes reachable from the root.  However, instead
- * of implementing the traversal recursively or iteratively with a stack, which
- * would take memory and thus defeat its purpose (Schorr-Waite is used for
+ * described in [1]. Briefly, the algorithm traverses the graph in depth-first
+ * search order and marks the nodes reachable from the root. However, instead of
+ * implementing the traversal recursively or iteratively with a stack, which
+ * would take memory and thus defeating its purpose (Schorr-Waite is used for
  * garbage collection), the Schorr-Waite algorithm uses the already existing
- * pointers in the graph to encode the stack.  During its operation, the graph
- * is therefore significantly altered, so it is important to prove that in
- * the end one obtains the original (reachable part of the) graph, but marked.
- *
+ * pointers in the graph to encode the stack. During its operation, the graph is
+ * therefore significantly altered, so it is important to prove that in the end
+ * one obtains the original (reachable part of the) graph, but marked.
+ * 
  * The graphNode structure uses the field mark to track how many times the
- * depth-first search traversal has visited the current node so far.
- * The valid values for mark are:
+ * depth-first search traversal has visited each node so far. The valid values
+ * for mark are:
  *   - 0 (the node is not visited yet)
  *   - 1 (the node's left subgraph is currently being visited)
  *   - 2 (the node's right subgraph is currently being visited)
@@ -20,28 +20,28 @@
  * traversal all the mark fields of nodes reachable from the root are set to 3.
  *
  * Here, we verify the implementation for the case when the part of the graph
- * reachable from the root is actually a tree.  Thus, the heap contains a tree,
+ * reachable from the root is actually a tree. Thus, the heap contains a tree,
  * while all the nodes unreachable from the root are part of the heap frame,
- * namely "...".  The mathematical Schorr-Waite tree is a tree of pairs
- * (pointer_to_node, mark).  In this context, pointers(T) refers to the tree
+ * namely "...". The mathematical Schorr-Waite tree is a tree of pairs
+ * (pointer_to_node, mark). In this context, pointers(T) refers to the tree
  * holding only the pointer component (dropping the mark component from each
  * node), while marks(T) refers to the tree holding only the mark component.
  * The specification states that
  *   - the mark component of the initial tree has all the elements set to 0
  *   - the mark component of the final tree has all the elements set to 3
  *   - the pointer components of the initial and final trees are the same.
- * In other words, the points-to structure of the tree stays unchanged when
- * the function terminates; what changes is only the mark status of the tree
- * (not marked before, but fully marked after).
- * 
+ * In other words, the points-to structure of the tree is unchanged when the
+ * function terminates; what is changed is only the marking of the tree (not
+ * marked before, but fully marked after).
+ *
  * The loop invariant states that the heap contains two trees rooted at p and q.
  * The isWellMarked predicate encodes that the values of the mark fields of the
  * nodes in the two trees respect the expected properties of an intermediate
- * state in the Schorr-Waite algorithm.  The invariant also states that the
+ * state in the Schorr-Waite algorithm. The invariant also states that the
  * pointer structure of the initial tree can be obtained by restoring the
- * pointers of the nodes in the two trees at p and q.  For the definitions
- * and axioms required by this proof see [2].
- *
+ * pointers of the nodes in the two trees at p and q. For the definitions and
+ * axioms required by this proof see [2].
+ * 
  * [1] http://www.cs.cornell.edu/courses/cs312/2007fa/lectures/lec21-schorr-waite.pdf
  * [2] http://code.google.com/p/matching-logic/source/browse/trunk/matchC/lib/theories/schorr-waite-theory.maude
  */
@@ -77,10 +77,12 @@ void schorr_waite(struct graphNode *root)
     struct graphNode *t;
 
     p->mark = p->mark + 1;
-    // The conditional below is written to clearly show the parallel assignments
-    // as in [1].  The reader is encouraged to refactor (and then verify) the
-    // conditional below by pulling the first three assignments before the
-    // conditional (don't forget to also modify the condition accordingly).
+    /*
+     * The conditional below is written to clearly show the parallel assignments
+     * as in [1]. The reader is encouraged to refactor (and then verify) the
+     * code below by pulling the first three assignments before the conditional
+     * (don't forget to also modify the condition accordingly).
+     */
     if (p->mark == 3 || p->left != NULL && p->left->mark == 0) {
       // parallel assignment p->left, p->right, q, p = p->right, q, p, p->left
       t = p->left;
