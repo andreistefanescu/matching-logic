e020f728eb7b1203b6649230244e215a49a51f29
naum.elena@gmail.com <naum.elena@gmail.com@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2011-09-08 11:48:34 +0000

    recursive dllist length

---
+++ trunk/matchC/developing_examples/double-linked-list/dllist_length_recursive.c
@@ -0,0 +1,24 @@
+/*
+ * Function computing recursively the length of a double linked list.
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct dllistNode {
+  int val;
+  struct dllistNode *prev;
+  struct dllistNode *next;
+};
+
+int length_recursive(struct dllistNode* a)
+/*@ rule <k> $ => return len(A); ...</k>
+         <heap>... dllist(a)(A) ...</heap> */
+{
+  if (a == 0) return 0;
+  return 1 + length(a->next);
+}
+
+
+//@ var A : Seq
