84bbfcacd9250a45b1985e0e9e18922d3af24623
brandon.3.moore <brandon.3.moore@gmail.com>
2012-10-18 22:10:09 +0000

    Execution and a proof with map patterns

--- coq/playground/imp-terms/domains.v
+++ coq/playground/imp-terms/domains.v
@@ -1,31 +1,26 @@
 Require Import BinPos.
 Require Import ZArith.
 Require Import FMapPositive.
+Require Import String.
 
 Set Implicit Arguments.
 
-Inductive Var := var (p : positive).
-Definition var_dec : forall (x y : Var), {x=y}+{x<>y}. 
-intros. decide equality. apply positive_eq_dec.
+Axiom Map : Set -> Set.
+Axiom mapItem : forall {A: Set}, A -> Z -> Map A.
+Axiom mapJoin : forall {A : Set}, Map A -> Map A -> Map A.
+Axiom mapEmpty : forall {A : Set}, Map A.
+Infix ":->" := mapItem (at level 50, no associativity).
+Infix ":*" := mapJoin (at level 60, right associativity).
+Axiom mapUnit : forall {A} (m : Map A), m :* mapEmpty = m.
+Axiom mapComm : forall {A} (m1 m2 : Map A), m1 :* m2 = m2 :* m1.
+Axiom mapAssoc : forall {A} (m1 m2 m3 : Map A), (m1 :* m2) :* m3 = m1 :* m2 :* m3.
+Lemma mapComAssoc : forall {A} (m1 m2 m3 : Map A), m1 :* m2 :* m3 = m2 :* m1 :* m3.
+intros. rewrite <- mapAssoc, (mapComm m1 m2), mapAssoc. reflexivity.
 Qed.
 
-Definition Val := Z.
-Definition Store := PositiveMap.t Val.
-Definition lookup (env : Store) (v : Var) : Z :=
-  match v with
-    | var key =>
-      match PositiveMap.find key env with
-        | Some x => x
-        | None => 0%Z
-      end
-  end.
-Definition update (env : Store) (v : Var) i :=
-  match v with
-    | var key => PositiveMap.add key i env
-  end.
-
+(* Language syntax *)
 Inductive Exp :=
-  | EVar (v:Var)
+  | EVar (v:string)
   | ECon (i:Z)
   | EPlus (l r:Exp)
   | EDiv (l r:Exp)
@@ -48,21 +43,14 @@ Definition isBool b :=
   end.
 Inductive Stmt :=
   | Skip
-  | SAssign (x:Var) (e:Exp)
+  | SAssign (x:string) (e:Exp)
+  | HAssign (e:Exp) (e2:Exp)
   | Seq (s1 s2 : Stmt)
   | SIf (b:BExp) (sthn sels : Stmt)
   | SWhile (b:BExp) (body:Stmt)
   .
-Inductive cfg :=
-  | Cfg : Stmt -> Store -> cfg
-  .
 
-Inductive sosCfg :=
-  | SCfg : Stmt -> Store -> sosCfg
-  | ECfg : Exp -> Store -> sosCfg
-  | BCfg : BExp -> Store -> sosCfg
-  .
-
+(* K definitions *)
 Inductive freezer : Set :=
   | Fplusl : Exp -> freezer
   | Fplusr : Exp -> freezer
@@ -71,7 +59,9 @@ Inductive freezer : Set :=
   | Flel : Exp -> freezer
   | Fler : Z -> freezer
   | Fandl : BExp -> freezer
-  | Fassign : Var -> freezer
+  | Fassign : string -> freezer
+  | Fhassignl : Exp -> freezer
+  | Fhassignr : Exp -> freezer
   | Fseq : Stmt -> freezer
   | Fif : Stmt -> Stmt -> freezer
   .
@@ -87,40 +77,4 @@ Definition kra : kitem -> list kitem -> list kitem := @cons kitem.
 Definition kdot : list kitem := nil.
 
 Inductive kcfg :=
-  | KCfg : list kitem -> Store -> kcfg.
-
-Section Ctxt.
-
-  Variable A : Set.
-  Inductive ctxt : Set -> Set :=
-  | hole : ctxt A
-  | plusl : ctxt Exp -> Exp -> ctxt Exp
-  | plusr : Exp -> ctxt Exp -> ctxt Exp
-  | divl : ctxt Exp -> Exp -> ctxt Exp
-  | divr : Exp -> ctxt Exp -> ctxt Exp
-  | lel : ctxt Exp -> Exp -> ctxt BExp
-  | ler : Z -> ctxt Exp -> ctxt BExp
-  | andl : ctxt BExp -> BExp -> ctxt BExp
-  | assigne : Var -> ctxt Exp -> ctxt Stmt
-  | seqc : ctxt Stmt -> Stmt -> ctxt Stmt
-  | ifc : ctxt BExp -> Stmt -> Stmt -> ctxt Stmt
-  | whilec : ctxt BExp -> Stmt -> ctxt Stmt
-  | cfgc : ctxt Stmt -> Store -> ctxt cfg
-  .
-  Fixpoint plug (R : Set) (c : ctxt R) (a : A) : R :=
-    match c with
-  | hole => a
-  | plusl cl r => EPlus (plug cl a) r
-  | plusr l cr => EPlus l (plug cr a)
-  | divl cl r => EDiv (plug cl a) r
-  | divr l cr => EDiv l (plug cr a)
-  | lel cl r => BLe (plug cl a) r
-  | ler i cr => BLe (ECon i) (plug cr a)
-  | andl cl r => BAnd (plug cl a) r
-  | assigne v ce => SAssign v (plug ce a)
-  | seqc cs s2 => Seq (plug cs a) s2
-  | ifc cc s1 s2 => SIf (plug cc a) s1 s2
-  | whilec cc b => SWhile (plug cc a) b
-  | cfgc cs env => Cfg (plug cs a) env
-  end.
-End Ctxt.
+  | KCfg : list kitem -> Map string -> Map Z -> kcfg.
