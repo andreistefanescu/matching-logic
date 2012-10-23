c696def7950f44483cfd645b1bfbdc2f18f4ee6e
brandon.3.moore@gmail.com <brandon.3.moore@gmail.com@9fa6ba1e-3f99-16e8-c2db-0e4dca8299a4>
2012-10-23 03:06:00 +0000

    Set svn:ignore propertly, extend language with labels

--- trunk/coq/playground/himp/kstyle.v
+++ trunk/coq/playground/himp/kstyle.v
@@ -8,14 +8,23 @@ Open Scope string_scope.
 
 Require Import domains.
 
+(*
 Notation "'□' '+' e" := (Fplusl e) (at level 50) : k_scope.
 Notation "e '+' '□'" := (Fplusr e) (at level 50) : k_scope.
-Notation "'□' '/' e" := (Fdivl e) (at level 40): k_scope.
-Notation "e '/' '□'" := (Fdivr e) (at level 40): k_scope.
-Notation "'□' '<=' e" := (Flel e) (at level 70): k_scope.
-Notation "i '<=' '□'" := (Fler i) (at level 70): k_scope.
-Notation "'□' '&&' e" := (Fandl e) (at level 40): k_scope.
-Notation "v ':=□'" := (Fassign v) (at level 30): k_scope.
+ *)
+Notation FreezeL f e := (KFreezeE (fun x => f x e)).
+Notation FreezeR f e := (KFreezeE (fun x => f e x)).
+
+Notation "'□' '+' e" := (FreezeL EPlus e) (at level 50) : k_scope.
+Notation "e '+' '□'" := (FreezeR EPlus e) (at level 50) : k_scope.
+Notation "'□' '-' e" := (FreezeL EMinus e) (at level 50) : k_scope.
+Notation "e '-' '□'" := (FreezeR EMinus e) (at level 50) : k_scope.
+Notation "'□/' e" := (FreezeL EDiv e) (at level 50): k_scope.
+Notation "e '/□'" := (FreezeR EDiv e) (at level 50): k_scope.
+Notation "'□' '<=' e" := (FreezeL BLe e) (at level 70): k_scope.
+Notation "i '<=' '□'" := (FreezeR BLe (ECon i)) (at level 70): k_scope.
+Notation "'□' '&&' e" := (KFreezeB (fun l => BAnd l e)) (at level 50): k_scope.
+Notation "v ':=□'" := (KFreezeE (fun e => SAssign v e)) (at level 70): k_scope.
 Notation "'if□then' s1 'else' s2" := (Fif s1 s2) (at level 20): k_scope.
 
 Delimit Scope k_scope with K.
@@ -28,23 +37,33 @@ Coercion KFreezer : freezer >-> kitem.
 
 Set Implicit Arguments.
 
-Notation exp1_step step a b :=
-  (forall rest env henv, step (KCfg (kra a rest) env henv) 
-                              (KCfg (kra b rest) env henv)) (only parsing).
 Notation exp_step step a b :=
-  (forall env henv, step (KCfg a env henv) (KCfg b env henv)) (only parsing).
+  (forall env henv lenv, step (KCfg a env henv lenv) (KCfg b env henv lenv)) (only parsing).
 
-Generalizable Variables rest env erest henv x y i j v r e b s.
+Notation exp1_step step a b := (forall rest, exp_step step (kra a rest) (kra b rest)) (only parsing).
+
+Notation heat_step step cool hot ctx :=
+  (forall rest, exp_step step (kra cool rest) (kra hot (kra ctx rest))).
+Notation cool_step step hot ctx cool :=
+  (forall rest, exp_step step (kra hot (kra ctx rest)) (kra cool rest)).
+
+Generalizable Variables rest env erest henv lenv x y i j v r e b s.
 Inductive kstep : kcfg -> kcfg -> Prop :=
   (* evaluation rules *)
-  | k_lookup : `(env = (x :-> i :* erest) ->
-                 kstep (KCfg (kra (EVar x) rest) env henv)
-                       (KCfg (kra (ECon i) rest) (x :-> i :* erest) henv))
-  | k_assign : `(env = (x :-> v :* erest) ->
-                 kstep (KCfg (kra (SAssign x (ECon i)) rest) env henv)
-                       (KCfg (kra Skip rest) (x :-> i :* erest) henv))
+  | k_lookup : `(env ~= (x :-> i :* erest) ->
+                 kstep (KCfg (kra (EVar x) rest) env henv lenv)
+                       (KCfg (kra (ECon i) rest) (x :-> i :* erest) henv lenv))
+  | k_assign : `(env ~= (x :-> v :* erest) ->
+                 kstep (KCfg (kra (SAssign x (ECon i)) rest) env henv lenv)
+                       (KCfg (kra Skip rest) (x :-> i :* erest) henv lenv))
+  | k_jump : `(forall l,
+                 lenv ~= (l :-> s :* erest) ->
+                 kstep (KCfg (kra (Jump l) rest) env henv lenv)
+                       (KCfg (kra s kdot) env henv lenv))
   | k_plus : `(exp1_step kstep (EPlus (ECon i) (ECon j))
                                (ECon (Zplus i j)))
+  | k_minus : `(exp1_step kstep (EMinus (ECon i) (ECon j))
+                                (ECon (Zminus i j)))
   | k_div : `(Zneq_bool 0%Z j = true ->
                exp1_step kstep (EDiv (ECon i) (ECon j))
                                (ECon (Zdiv i j)))
@@ -63,133 +82,141 @@ Inductive kstep : kcfg -> kcfg -> Prop :=
   | k_while : `(exp1_step kstep (SWhile b s)
                                 (SIf b (Seq s (SWhile b s)) Skip))
   (* heating and cooling *)
+  | k_cool_e : `(cool_step kstep (ECon i) (KFreezeE e) (e (ECon i)))
   (* plus *)
-  | k_heat_plus_l : `(isEVal e = false ->
-                exp_step kstep (kra (EPlus e e2) rest)
-                               (kra e (kra (□ + e2) rest)))
-  | k_heat_plus_r : `(isEVal e2 = false ->
-                exp_step kstep (kra (EPlus e e2) rest)
-                               (kra e2 (kra (e + □) rest)))
-  | k_cool_plus_l : `(exp_step kstep (kra (ECon i) (kra (□ + e) rest))
-                                     (kra (EPlus (ECon i) e) rest))
-  | k_cool_plus_r : `(exp_step kstep (kra (ECon i) (kra (e + □) rest))
-                                     (kra (EPlus e (ECon i)) rest))
+  | k_heat_plus_l : `(isEVal e = false ->  heat_step kstep (EPlus e e2) e  (□ + e2))
+  | k_heat_plus_r : `(isEVal e2 = false -> heat_step kstep (EPlus e e2) e2 (e + □))
+(*
+  | k_cool_plus_l : `(cool_step kstep (ECon i) (□ + e) (EPlus (ECon i) e))
+  | k_cool_plus_r : `(cool_step kstep (ECon i) (e + □) (EPlus e (ECon i)))
+ *)
+  (* minus *)
+  | k_heat_minus_l : `(isEVal e = false ->  heat_step kstep (EMinus e e2) e  (□ - e2))
+  | k_heat_minus_r : `(isEVal e2 = false -> heat_step kstep (EMinus e e2) e2 (e - □))
+(*
+  | k_cool_minus_l : `(cool_step kstep (ECon i) (□ - e) (EMinus (ECon i) e))
+  | k_cool_minus_r : `(cool_step kstep (ECon i) (e - □) (EMinus e (ECon i)))
+*)
   (* div *)
-  | k_heat_div_l : `(isEVal e = false ->
-                exp_step kstep (kra (EDiv e e2) rest)
-                               (kra e (kra (□ / e2) rest)))
-  | k_heat_div_r : `(isEVal e2 = false ->
-                exp_step kstep (kra (EDiv e e2) rest)
-                               (kra e2 (kra (e / □) rest)))
-  | k_cool_div_l : `(exp_step kstep (kra (ECon i) (kra (□ / e) rest))
-                                    (kra (EDiv (ECon i) e) rest))
-  | k_cool_div_r : `(exp_step kstep (kra (ECon i) (kra (e / □) rest))
-                                    (kra (EDiv e (ECon i)) rest))
+  | k_heat_div_l : `(isEVal e = false ->  heat_step kstep (EDiv e e2) e (□/ e2))
+  | k_heat_div_r : `(isEVal e2 = false -> heat_step kstep (EDiv e e2) e2 (e /□))
+(*
+  | k_cool_div_l : `(cool_step kstep (ECon i) (□/ e) (EDiv (ECon i) e))
+  | k_cool_div_r : `(cool_step kstep (ECon i) (e /□) (EDiv e (ECon i)))
+ *)
   (* le is seqstrict *)
-  | k_heat_le_l : `(isEVal e = false ->
-                exp_step kstep (kra (BLe e e2) rest)
-                               (kra e (kra (□ <= e2) rest)))
-  | k_heat_le_r : `(isEVal e2 = false ->
-                exp_step kstep (kra (BLe (ECon i) e2) rest)
-                               (kra e2 (kra (i <= □) rest)))
-  | k_cool_le_l : `(exp_step kstep (kra (ECon i) (kra (□ <= e) rest))
-                                   (kra (BLe (ECon i) e) rest))
-  | k_cool_le_r : `(exp_step kstep (kra (ECon j) (kra (i <= □) rest))
-                                   (kra (BLe (ECon i) (ECon j)) rest))
-  (* and only left-strict *)
-  | k_heat_and : `(isBool b1 = false ->
-                 exp_step kstep (kra (BAnd b1 b2) rest)
-                                (kra b1 (kra (□ && b2) rest)))
-  | k_cool_and : `(exp_step kstep (kra (BCon b) (kra (□ && b2) rest))
-                                  (kra (BAnd (BCon b) b2) rest))
+  | k_heat_le_l : `(isEVal e = false ->  heat_step kstep (BLe e e2) e (□ <= e2))
+  | k_heat_le_r : `(isEVal e2 = false -> heat_step kstep (BLe (ECon i) e2) e2 (i <= □))
+(*
+  | k_cool_le_l : `(cool_step kstep (ECon i) (□ <= e) (BLe (ECon i) e))
+  | k_cool_le_r : `(cool_step kstep (ECon j) (i <= □) (BLe (ECon i) (ECon j)))
+ *)
+  (* and is only strict in left argument *)
+  | k_heat_and : `(isBool b1 = false -> heat_step kstep (BAnd b1 b2) b1 (□ && b2))
+(*
+  | k_cool_and : `(cool_step kstep (BCon b) (□ && b2) (BAnd (BCon b) b2))
+ *)
   (* assign *)
-  | k_heat_assign : `(isEVal e = false ->
-                  exp_step kstep (kra (SAssign x e) rest)
-                                 (kra e (kra (x :=□) rest)))
-  | k_cool_assign : `(exp_step kstep (kra (ECon i) (kra (x :=□) rest))
-                                     (kra (SAssign x (ECon i)) rest))
-
+  | k_heat_assign : `(isEVal e = false -> heat_step kstep (SAssign x e) e (x :=□))
+(*
+  | k_cool_assign : `(cool_step kstep (ECon i) (x :=□) (SAssign x (ECon i)))
+ *)
   (* if *)
-  | k_heat_if : `(isBool b = false ->
-               exp_step kstep (kra (SIf b s1 s2) rest)
-                              (kra b (kra (if□then s1 else s2) rest)))
-  | k_cool_if : `(
-               exp_step kstep (kra (BCon b) (kra (if□then s1 else s2) rest))
-                              (kra (SIf (BCon b) s1 s2) rest))
+  | k_heat_if : `(isBool b = false -> heat_step kstep (SIf b s1 s2) b (if□then s1 else s2))
+  | k_cool_if : `(cool_step kstep (BCon b) (if□then s1 else s2) (SIf (BCon b) s1 s2))
   (* seq *)
-  | k_heat_seq : `(exp_step kstep (kra (Seq s1 s2) rest)
-                                  (kra s1 (kra s2 rest)))
+  | k_heat_seq : `(heat_step kstep (Seq s1 s2) s1 s2)
   .
 
+Definition kequiv (k1 k2 : kcfg) : Prop :=
+  match k1, k2 with
+    | KCfg k1 store1 heap1 labels1,
+      KCfg k2 store2 heap2 labels2 =>
+      k1 = k2 /\ store1 ~= store2 /\ heap1 ~= heap2 /\ labels1 ~= labels2
+  end.
+
 CoInductive steps : kcfg -> kcfg -> Prop :=
-  | done : forall c1 c2, c1 = c2 -> steps c1 c2
+  | done : forall c1 c2, kequiv c1 c2 -> steps c1 c2
   | more : forall c1 c2 c3, kstep c1 c2 -> steps c2 c3 -> steps c1 c3
   .
 
 Ltac find x map k :=
   match map with
-    | (x :-> _ :* _) => let pf := constr:(eq_refl map) in k pf
-    | (?mapl :* (x :-> ?v)) => let pf := constr:(mapComm mapl (x :-> v)) in k pf
+    | (x :-> _ :* _) => let pf := constr:(equivRefl map) in k pf
+    | (?mapl :* (x :-> ?v)) => let pf := constr:(equivComm mapl (x :-> v)) in k pf
     | ?mapl :* ?mapr =>
-           find x mapl ltac:(fun pfl => let pf := constr:(eq_trans (f_equal (fun m => m :* mapr) pfl)
-                                                                   (mapAssoc (x :-> _) _ mapr))
+           find x mapl ltac:(fun pfl => let pf := constr:(equivTrans (equivJoinL mapr pfl)
+                                                                   (equivAssoc (x :-> _) _ mapr))
                                         in k pf)
-        || find x mapr ltac:(fun pfr => let pf := constr:(eq_trans (f_equal (fun m => mapl :* m) pfr)
+        || find x mapr ltac:(fun pfr => let pf := constr:(equivTrans (equivJoinR mapl pfr)
                                                                    (mapComAssoc mapl (x :-> _) _))
                                         in k pf)
   end.
 Ltac find_submap map submap k :=
   match map with
-    | (submap :* _) => let pf := constr:(eq_refl map) in k pf
-    | (?mapl :* submap) => let pf := constr:(mapComm mapl submap) in k pf
+    | (submap :* _) => let pf := constr:(equivRefl map) in k pf
+    | (?mapl :* submap) => let pf := constr:(equivComm mapl submap) in k pf
     | ?mapl :* ?mapr =>
            find_submap mapl submap
-                       ltac:(fun pfl => let pf := constr:(eq_trans (f_equal (fun m => m :* mapr) pfl)
-                                                                   (mapAssoc submap _ mapr))
+
+                       ltac:(fun pfl => let pf := constr:(equivTrans (equivJoinL mapr pfl)
+                                                                   (equivAssoc submap _ mapr))
                                         in k pf)
         || find_submap mapr submap
-                       ltac:(fun pfr => let pf := constr:(eq_trans (f_equal (fun m => mapl :* m) pfr)
+                       ltac:(fun pfr => let pf := constr:(equivTrans (equivJoinR mapl pfr)
                                                                    (mapComAssoc mapl submap _))
                                         in k pf)
   end.
 
 Ltac find_map_entry :=
   match goal with
-    | [|- ?x :-> ?v = ?x :-> _ :* _] => rewrite <- (mapUnit (x :-> v)); reflexivity
-    | [|- ?map = ?x :-> ?v :* ?map2 ] =>
-      find x map ltac:(fun pf => exact pf)
+    | [|- ?map ~= _ ] =>
+      ((is_var map;
+       match goal with
+         | [H : ?map ~= ?mapr |- ?map ~= _] =>
+           match mapr with
+             | map => fail
+             | _ => rewrite H
+           end
+       end)
+        || (try (unfold map)));
+      match goal with
+        | [|- ?x :-> ?v ~= ?x :-> _ :* _] => rewrite (equivUnit (x :-> v)); reflexivity
+        | [|- ?map ~= ?x :-> ?v :* ?map2 ] =>
+          find x map ltac:(fun pf => exact pf)
+      end
   end.
 
-Lemma mapUnitL : forall {A} (m : Map A), mapEmpty :* m = m.
-intros. rewrite mapComm. apply mapUnit.
+Lemma equivUnitL : forall {K V} (m : Map K V), MapEquiv (mapEmpty :* m) m.
+intros. rewrite equivComm. apply equivUnit.
 Qed.
 
-Ltac equate_maps := rewrite ?mapAssoc, ?mapUnitL, ?mapUnit;
+Ltac equate_maps := rewrite ?equivAssoc, ?equivUnitL, ?equivUnit;
  repeat (rewrite ?f_equal;
          match goal with
-           | [|- (?x :-> _ :* _) = (?x :-> _ :* _)] => apply f_equal2;[apply f_equal;omega|]
-           | [|-?map = ?x :-> _ :* _] => find x map ltac:(fun pf => rewrite pf)
-           | [|-?map = ?submap :* _] => find_submap map submap ltac:(fun pf => rewrite pf)
-           | [|- ?map = ?map ] => reflexivity
+           | [|- (?x :-> ?v1 :* _) ~= (?x :-> ?v2 :* _)] => apply equivJoin;[replace v1 with v2 by omega|]
+           | [|- MapEquiv ?map (?x :-> _ :* _)] => find x map ltac:(fun pf => rewrite pf)
+           | [|- MapEquiv ?map (?submap :* _)] => find_submap map submap ltac:(fun pf => rewrite pf)
+           | [|- MapEquiv ?map ?map ] => reflexivity
          end).
 
 Ltac finish :=
-  apply done; apply (f_equal2 (KCfg _)); equate_maps.
+  apply done;repeat (apply conj);[reflexivity|simpl;equate_maps ..].
 
 Lemma eval_happy : forall env,
   steps (KCfg (kra (SAssign "x" (EPlus (EVar "x") (EVar "y"))) nil)
-            (env :* "x" :-> 12%Z :* "y" :-> 13%Z) mapEmpty)
-        (KCfg nil (env :* "x" :-> 25%Z :* "y" :-> 13%Z) mapEmpty).
+            (env :* "x" :-> 12%Z :* "y" :-> 13%Z) mapEmpty mapEmpty)
+        (KCfg nil (env :* "x" :-> 25%Z :* "y" :-> 13%Z) mapEmpty mapEmpty).
 intros;
 repeat (eapply more;[econstructor (reflexivity || find_map_entry)|]).
-simpl Zplus;finish.
+simpl Zplus; finish.
 Qed.
 
 Lemma loop_test:
   steps (KCfg (kra (SWhile (BLe (ECon 0) (EVar "x"))
     (SAssign "x" (EPlus (EVar "x") (ECon (-1)%Z))))
-  nil) ("x" :-> 25%Z) mapEmpty)
-  (KCfg nil ("x" :-> (-1)%Z) mapEmpty).
+  nil) ("x" :-> 25%Z) mapEmpty mapEmpty)
+  (KCfg nil ("x" :-> (-1)%Z) mapEmpty mapEmpty).
 Proof.
 intros;
 repeat (eapply more;[econstructor (reflexivity || find_map_entry)|];instantiate;simpl Zplus);
@@ -199,31 +226,54 @@ Qed.
 Ltac step_rule := eapply more;[econstructor (reflexivity || find_map_entry)|].
 Ltac split_if :=
   match goal with
-      | [|- steps (KCfg (kra (KStmt (SIf (BCon (?x <=? ?y)%Z) _ _)) _) _ _) _] =>
+      | [|- steps (KCfg (kra (KStmt (SIf (BCon (?x <=? ?y)%Z) _ _)) _) _ _ _) _] =>
         pose proof (Zle_cases x y); destruct (x <=? y)%Z
   end.
 
+(* Either solve immediately by using the circularity,
+   or bail out for manual intervention if the
+   circularity matches structurally *)
 Ltac use_circ circ :=
-  eapply circ;
-  try (match goal with [|- @eq (Map string) _ _] => equate_maps;reflexivity end);
-  instantiate;  omega.
+  solve[eapply circ;
+  try (match goal with [|- _ ~= _] => equate_maps;reflexivity end);
+  instantiate;  omega]
+  || (eapply circ;fail 1).
 
 Ltac run_circ circ := repeat (use_circ circ || (step_rule || split_if || finish)).
 
 CoFixpoint sum :
-  forall (x y z : Z) erest env heap,
+  forall (x y z : Z) erest env heap labels,
     (0 <= x)%Z ->
     z = (x + y)%Z ->
-    env = ("i" :-> x :* "j" :-> y :* erest) ->
+    env ~= ("i" :-> x :* "j" :-> y :* erest) ->
   steps
     (KCfg (kra (SWhile (BLe (ECon 1%Z) (EVar "i"))
                        (Seq
                           (SAssign "i" (EPlus (EVar "i") (ECon (-1)%Z)))
                           (SAssign "j" (EPlus (EVar "j") (ECon 1%Z)))))
                kdot)
-                env heap)
+                env heap labels)
     (KCfg kdot
-          ("i" :-> 0%Z :* "j" :-> z%Z :* erest) heap).
-intros. subst env.
-step_rule; run_circ sum.
+          ("i" :-> 0%Z :* "j" :-> z%Z :* erest) heap labels).
+intros;step_rule;run_circ sum.
+Qed.
+
+Coercion EVar : string >-> Exp.
+
+Definition gcdProg : Map string Stmt :=
+  "gcd" :->
+        SIf (BLe "a" "b")
+          (SIf (BLe "b" "a")
+               (Jump "done")
+               (Seq (SAssign "b" (EMinus "b" "a"))
+                    (Jump "gcd")))
+          (Seq (SAssign "a" (EMinus "a" "b"))
+               (Jump "gcd")).
+
+Lemma label_eval : forall env,
+  steps (KCfg (kra (Jump "gcd") kdot)
+            (env :* "a" :-> 12%Z :* "b" :-> 13%Z) mapEmpty gcdProg)
+        (KCfg (kra (Jump "done") kdot)
+             (env :* "a" :-> 1%Z :* "b" :-> 1%Z) mapEmpty gcdProg).
+intros; do 307 step_rule;simpl Zminus; finish.
 Qed.
