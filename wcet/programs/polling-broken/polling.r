4da4cbc70ba57231fb03886c1c5af3241ed3f0ca
dwight.guth <dwight.guth@gmail.com>
2012-12-15 20:51:56 +0000

    prove polling server, running time of sum

--- wcet/programs/polling-broken/polling.r
+++ wcet/programs/polling-broken/polling.r
@@ -2,68 +2,8 @@
   <lhs>
     <config>
       <T>
-        <k> load(
-main:	store #0, #0
-	li r 0 , # #symInt("N")
-        li r 1 , #0
-        li r 2 , #0
-	jmp loop
-loop:	rw r 1, data , #0
-	load r 2 , #0
-	add r 2 , r 2 , r 1
-	store #0, r 2
-	sub r 0 , r 0 , #1
-	bne loop , r 0 , #0
-	halt
-) ~> jumpToMain </k>
-        <pgm> .Map </pgm>
-        <mem> .Map </mem>
-        <reg> .Map </reg>
-        <timing> add |-> 1 sub |-> 1 mul |-> 1 div |-> 1 or |-> 1 and |-> 1 not |-> 1 load |-> 3 store |-> 3 jmp |-> 1 beq |-> 2 bne |-> 2 blt |-> 2 ble |-> 2 halt |-> 1 read |-> 3 write |-> 3 rw |-> 6 </timing>
-        <wcet> 0 </wcet>
-        <input> ListItem((#symInt("Time1"), data |-> #symInt("Data1"))) ListItem((#symInt("Time2"), data |-> #symInt("Data2"))) </input>
-        <status> data |-> #symInt("Data") </status>
-      </T>
-    </config>
-    <formula> #symInt("Time2") -Int #symInt("Time1") >Int 16 andBool 0 <Int #symInt("Time1") andBool (#symInt("N") -Int 1) *Int 16 >Int #symInt("Time2") andBool #symInt("Time1") >Int 7 </formula>
-    <progress> false </progress>
-  </lhs>
-  <rhss>
-    <rhs>
-      <rconfig>
-        <T>
-          <k> .K </k>
-          <pgm> 
-            main |-> store #0, #0
-                     or r 0 , # #symInt("N") , #0
-                     or r 1 , #0 , #0
-                     or r 2 , #0 , #0
-                     jmp loop
-            loop |-> rw r 1 , data , #0
-                     load r 2 , #0
-                     add r 2 , r 2 , r 1
-                     store #0, r 2
-                     sub r 0 , r 0 , #1
-                     bne loop , r 0 , #0
-                     halt </pgm>
-          <reg> 0 |-> 0 1 |-> #symInt("GeneratedFreshVar2") 2 |-> #symInt("GeneratedFreshVar3") </reg>
-          <mem> 0 |-> #symInt("Data") +Int #symInt("Data1") +Int #symInt("Data2") </mem>
-          <status> data |-> 0 </status>
-          <input> .List </input>
-          <timing> add |-> 1 sub |-> 1 mul |-> 1 div |-> 1 or |-> 1 and |-> 1 not |-> 1 load |-> 3 store |-> 3 jmp |-> 1 beq |-> 2 bne |-> 2 blt |-> 2 ble |-> 2 halt |-> 1 read |-> 3 write |-> 3 rw |-> 6 </timing>
-          <wcet> #symInt("N") *Int 16 +Int 8 </wcet>
-        </T>     
-      </rconfig>
-      <rformula> true </rformula>
-      <rfreevars> SetItem(#symInt("GeneratedFreshVar2")) SetItem(#symInt("GeneratedFreshVar3")) </rfreevars>
-    </rhs>
-  </rhss>
-</task>
-<task>
-  <lhs>
-    <config>
-      <T>
-        <k> rw r 1 , data , #0
+        <k> read r 1 , data
+            write data , #0
             load r 2 , #0
             add r 2 , r 2 , r 1
             store #0, r 2
@@ -72,11 +12,10 @@ loop:	rw r 1, data , #0
             halt </k>
         <pgm> 
           main |-> store #0, #0
-                   or r 0 , # #symInt("_") , #0
-                   or r 1 , #0 , #0
-                   or r 2 , #0 , #0
+                   or r 0 , #100 , #0
                    jmp loop
-          loop |-> rw r 1 , data , #0
+          loop |-> read r 1 , data
+                   write data , #0
                    load r 2 , #0
                    add r 2 , r 2 , r 1
                    store #0, r 2
@@ -101,11 +40,10 @@ loop:	rw r 1, data , #0
           <k> .K </k>
           <pgm> 
             main |-> store #0, #0
-                     or r 0 , # #symInt("_") , #0
-                     or r 1 , #0 , #0
-                     or r 2 , #0 , #0
+                     or r 0 , #100 , #0
                      jmp loop
-            loop |-> rw r 1 , data , #0
+            loop |-> read r 1 , data
+                     write data , #0
                      load r 2 , #0
                      add r 2 , r 2 , r 1
                      store #0, r 2
@@ -129,7 +67,8 @@ loop:	rw r 1, data , #0
   <lhs>
     <config>
       <T>
-        <k> rw r 1 , data , #0
+        <k> read r 1 , data
+            write data , #0
             load r 2 , #0
             add r 2 , r 2 , r 1
             store #0, r 2
@@ -138,11 +77,10 @@ loop:	rw r 1, data , #0
             halt </k>
         <pgm> 
           main |-> store #0, #0
-                   or r 0 , # #symInt("_") , #0
-                   or r 1 , #0 , #0
-                   or r 2 , #0 , #0
+                   or r 0 , #100 , #0
                    jmp loop
-          loop |-> rw r 1 , data , #0
+          loop |-> read r 1 , data
+                   write data , #0
                    load r 2 , #0
                    add r 2 , r 2 , r 1
                    store #0, r 2
@@ -167,11 +105,10 @@ loop:	rw r 1, data , #0
           <k> .K </k>
           <pgm> 
             main |-> store #0, #0
-                     or r 0 , # #symInt("_") , #0
-                     or r 1 , #0 , #0
-                     or r 2 , #0 , #0
+                     or r 0 , #100 , #0
                      jmp loop
-            loop |-> rw r 1 , data , #0
+            loop |-> read r 1 , data
+                     write data , #0
                      load r 2 , #0
                      add r 2 , r 2 , r 1
                      store #0, r 2
@@ -195,7 +132,8 @@ loop:	rw r 1, data , #0
   <lhs>
     <config>
       <T>
-        <k> rw r 1 , data , #0
+        <k> read r 1 , data
+            write data , #0
             load r 2 , #0
             add r 2 , r 2 , r 1
             store #0, r 2
@@ -204,11 +142,10 @@ loop:	rw r 1, data , #0
             halt </k>
         <pgm> 
           main |-> store #0, #0
-                   or r 0 , # #symInt("_") , #0
-                   or r 1 , #0 , #0
-                   or r 2 , #0 , #0
+                   or r 0 , #100 , #0
                    jmp loop
-          loop |-> rw r 1 , data , #0
+          loop |-> read r 1 , data
+                   write data , #0
                    load r 2 , #0
                    add r 2 , r 2 , r 1
                    store #0, r 2
@@ -233,11 +170,10 @@ loop:	rw r 1, data , #0
           <k> .K </k>
           <pgm> 
             main |-> store #0, #0
-                     or r 0 , # #symInt("_") , #0
-                     or r 1 , #0 , #0
-                     or r 2 , #0 , #0
+                     or r 0 , #100 , #0
                      jmp loop
-            loop |-> rw r 1 , data , #0
+            loop |-> read r 1 , data
+                     write data , #0
                      load r 2 , #0
                      add r 2 , r 2 , r 1
                      store #0, r 2
--- wcet/programs/sum/sum.r
+++ wcet/programs/sum/sum.r
@@ -2,35 +2,34 @@
   <lhs>
     <config>
       <T>
-        <k> add r 2 , r 2 , #1
-            add r 1 , r 1 , r 2
-            bne sum , r 2 , r 0
-            halt </k>
-        <pgm> main |-> li r 0 , #10
-                       li r 1 , #0
-                       li r 2 , #0
-                       jmp sum
-              sum |-> add r 2 , r 2 , #1
-                      add r 1 , r 1 , r 2
-                      bne sum , r 2 , r 0
-                      halt </pgm>
+        <k> load(
+main:	li r 0 , # #symInt("N")
+	li r 1 , #0
+	li r 2 , #0
+	jmp sum
+sum:	add r 2 , r 2 , #1
+	add r 1 , r 1 , r 2
+	bne sum , r 2 , r 0
+	halt
+) ~> jumpToMain </k>
+        <pgm> .Map </pgm>
         <mem> .Map </mem>
-        <status> .Map </status>
-        <input> .Map </input>
-        <reg> 0 |-> #symInt("N") 1 |-> #symInt("S") 2 |-> #symInt("I") </reg>
+        <reg> .Map </reg>
         <timing> add |-> 1 sub |-> 1 mul |-> 1 div |-> 1 or |-> 1 and |-> 1 not |-> 1 load |-> 10 store |-> 10 jmp |-> 1 beq |-> 2 bne |-> 2 blt |-> 2 ble |-> 2 halt |-> 1 read |-> 10 </timing>
-        <wcet> #symInt("GeneratedFreshVar0") </wcet>
+        <wcet> 0 </wcet>
+        <input> .List </input>
+        <status> .Map </status>
       </T>
     </config>
-    <formula> #symInt("S") ==Int (#symInt("I") *Int (#symInt("I") +Int 1) /Int 2) </formula>
+    <formula> true </formula>
     <progress> false </progress>
   </lhs>
   <rhss>
     <rhs>
-      <rconfig>
+       <rconfig>
         <T>
-          <k> halt </k>
-          <pgm> main |-> li r 0 , #10
+          <k> .K </k>
+          <pgm> main |-> li r 0 , # #symInt("N")
                          li r 1 , #0
                          li r 2 , #0
                          jmp sum
@@ -40,14 +39,67 @@
                         halt </pgm>
           <mem> .Map </mem>
           <status> .Map </status>
-          <input> .Map </input>
-          <reg> 0 |-> #symInt("N") 1 |-> (#symInt("N") *Int (#symInt("N") +Int 1) /Int 2) 2 |-> #symInt("N") </reg>
+          <input> .List </input>
+          <reg> 0 |-> #symInt("N") 1 |-> #symInt("N") *Int (#symInt("N") +Int 1) divInt 2  2 |-> #symInt("N") </reg>
           <timing> add |-> 1 sub |-> 1 mul |-> 1 div |-> 1 or |-> 1 and |-> 1 not |-> 1 load |-> 10 store |-> 10 jmp |-> 1 beq |-> 2 bne |-> 2 blt |-> 2 ble |-> 2 halt |-> 1 read |-> 10 </timing>
-          <wcet> #symInt("GeneratedFreshVar1") </wcet>
+          <wcet> 5 +Int 4 *Int #symInt("N") </wcet>
         </T>
       </rconfig>
       <rformula> true </rformula>
-      <rfreevars> SetItem(#symInt("GeneratedFreshVar1")) </rfreevars>
+      <rfreevars> .Set </rfreevars>
+    </rhs>
+  </rhss>
+</task>
+<task>
+  <lhs>
+    <config>
+      <T>
+        <k> add r 2 , r 2 , #1
+            add r 1 , r 1 , r 2
+            bne sum , r 2 , r 0
+            halt </k>
+        <pgm> main |-> li r 0 , # #symInt("_")
+                       li r 1 , #0
+                       li r 2 , #0
+                       jmp sum
+              sum |-> add r 2 , r 2 , #1
+                      add r 1 , r 1 , r 2
+                      bne sum , r 2 , r 0
+                      halt </pgm>
+        <mem> .Map </mem>
+        <status> .Map </status>
+        <input> .List </input>
+        <reg> 0 |-> #symInt("N") 1 |-> #symInt("S") 2 |-> #symInt("I") </reg>
+        <timing> add |-> 1 sub |-> 1 mul |-> 1 div |-> 1 or |-> 1 and |-> 1 not |-> 1 load |-> 10 store |-> 10 jmp |-> 1 beq |-> 2 bne |-> 2 blt |-> 2 ble |-> 2 halt |-> 1 read |-> 10 </timing>
+        <wcet> #symInt("Time") </wcet>
+      </T>
+    </config>
+    <formula> #symInt("S") ==Int (#symInt("I") *Int (#symInt("I") +Int 1) divInt 2) </formula>
+    <progress> false </progress>
+  </lhs>
+  <rhss>
+    <rhs>
+      <rconfig>
+        <T>
+          <k> .K </k>
+          <pgm> main |-> li r 0 , # #symInt("_")
+                         li r 1 , #0
+                         li r 2 , #0
+                         jmp sum
+                sum |-> add r 2 , r 2 , #1
+                        add r 1 , r 1 , r 2
+                        bne sum , r 2 , r 0
+                        halt </pgm>
+          <mem> .Map </mem>
+          <status> .Map </status>
+          <input> .List </input>
+          <reg> 0 |-> #symInt("N") 1 |-> (#symInt("N") *Int (#symInt("N") +Int 1) divInt 2) 2 |-> #symInt("N") </reg>
+          <timing> add |-> 1 sub |-> 1 mul |-> 1 div |-> 1 or |-> 1 and |-> 1 not |-> 1 load |-> 10 store |-> 10 jmp |-> 1 beq |-> 2 bne |-> 2 blt |-> 2 ble |-> 2 halt |-> 1 read |-> 10 </timing>
+          <wcet> #symInt("Time") +Int (#symInt("N") -Int #symInt("I")) *Int 4 +Int 1 </wcet>
+        </T>
+      </rconfig>
+      <rformula> true </rformula>
+      <rfreevars> .Set </rfreevars>
     </rhs>
   </rhss>
 </task>
